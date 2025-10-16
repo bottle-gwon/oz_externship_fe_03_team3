c6a5cef71cc46221774600522009888975ae77b8
bottle-gwon <lazyguy1@daum.net>
2025-10-16 15:14:28 +0900

    feat: 태그 페이지네이션 인터페이스 생성

--- src/components/recruit/write/tagSelectModal/feat/TagList.tsx
+++ src/components/recruit/write/tagSelectModal/feat/TagList.tsx
@@ -1,5 +1,13 @@
 import TagCard from './TagCard'
 
+// 페이지 네이션 타입
+export interface TagPaginationInterface {
+  currentPage: number
+  totalPage: number
+  onPageChange: (newPage: number) => void // 페이지 전환 함수 정의
+  maxPage?: number
+}
+
 const TagList = () => {
   return (
     <div>
---
+++ src/components/recruit/write/tagSelectModal/feat/TagPagination.tsx
@@ -0,0 +1,12 @@
+import type { TagPaginationInterface } from './TagList'
+
+const TagPagination = ({
+  currentPage,
+  totalPage,
+  onPageChange,
+  maxPage = 5,
+}: TagPaginationInterface) => {
+  return <div></div>
+}
+
+export default TagPagination
