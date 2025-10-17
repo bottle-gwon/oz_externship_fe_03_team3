9da357581db587504f68be9436358380cebf50fd
nari-0v0 <ej9811@naver.com>
2025-10-17 12:49:22 +0900

    feat: 구인목록카드 컴포넌트 생성 #14

---
+++ src/components/recruit/RecruitCard.tsx
@@ -0,0 +1,18 @@
+// 큰 박스
+;<div className="outBox">
+  <div aria-label="이미지" className="imgBox"></div>
+
+  <div>
+    <div aria-label="제목" className="title"></div>
+    <div aria-label="모집인원수" className="expected_personnel"></div>
+    <div aria-label="마감일" className="due_date"></div>
+    <div aria-label="강의제목" className="lectures"></div>
+    <div aria-label="강의목록" className="description"></div>
+    <div aria-label="태그" className="tags"></div>
+  </div>
+
+  <div>
+    <div aria-label="조회수" className="viewCount"></div>
+    <div aria-label="북마크" className="bookmarkCount"></div>
+  </div>
+</div>
