0b0e05e90b892ef294146dfb2104c865e6908726
Tynahan <nusilite@gmail.com>
2025-10-17 11:14:02 +0900

    feat: select 옵션들이 공중에 뜨도록 수정 #18

--- src/components/commonInGeneral/select/Select.tsx
+++ src/components/commonInGeneral/select/Select.tsx
@@ -1,4 +1,10 @@
-import { createContext, useContext, useState, type ReactNode } from 'react'
+import {
+  createContext,
+  useContext,
+  useRef,
+  useState,
+  type ReactNode,
+} from 'react'
 import { Hstack, Vstack } from '../layout'
 import RoundBox from '../roundBox/RoundBox'
 import { ChevronDown } from 'lucide-react'
@@ -8,6 +14,7 @@ interface SelectContextProps {
   setIsOpened: React.Dispatch<React.SetStateAction<boolean>>
   selectedItem: string | null
   setSelectedItem: React.Dispatch<React.SetStateAction<string | null>>
+  triggerRef: React.RefObject<HTMLDivElement | null>
 }
 
 const SelectContext = createContext<SelectContextProps | null>(null)
@@ -21,14 +28,14 @@ const useSelectContext = () => {
 }
 
 const SelectTrigger = ({ children }: { children: string }) => {
-  const { setIsOpened, selectedItem } = useSelectContext()
+  const { setIsOpened, selectedItem, triggerRef } = useSelectContext()
   const handleClick = () => {
     setIsOpened((prev) => !prev)
   }
 
   const label = selectedItem ?? children
   return (
-    <RoundBox onClick={handleClick}>
+    <RoundBox ref={triggerRef} onClick={handleClick}>
       <Hstack>
         <Hstack className="grow">{label}</Hstack>
         <ChevronDown />
@@ -57,14 +64,22 @@ const SelectItem = ({
 }
 
 const SelectContent = ({ children }: { children: ReactNode }) => {
-  const { isOpened } = useSelectContext()
+  const { isOpened, triggerRef } = useSelectContext()
 
   if (!isOpened) {
     return null
   }
 
+  if (!triggerRef.current) {
+    return null
+  }
+
   return (
-    <RoundBox padding="xs">
+    <RoundBox
+      style={{ top: triggerRef.current.offsetHeight + 4 }}
+      padding="xs"
+      className="absolute w-full"
+    >
       <Vstack gap="none">{children}</Vstack>
     </RoundBox>
   )
@@ -73,10 +88,17 @@ const SelectContent = ({ children }: { children: ReactNode }) => {
 const Select = ({ children }: { children: ReactNode }) => {
   const [isOpened, setIsOpened] = useState<boolean>(false)
   const [selectedItem, setSelectedItem] = useState<string | null>(null)
+  const triggerRef = useRef<HTMLDivElement>(null)
 
   return (
     <SelectContext
-      value={{ isOpened, setIsOpened, selectedItem, setSelectedItem }}
+      value={{
+        isOpened,
+        setIsOpened,
+        selectedItem,
+        setSelectedItem,
+        triggerRef,
+      }}
     >
       <Vstack gap="xs" className="relative gap-0">
         {children}
