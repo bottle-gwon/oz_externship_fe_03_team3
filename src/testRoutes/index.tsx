fb05c360ebbb435002da8be01c19b40c6550d8d0
nari-0v0 <ej9811@naver.com>
2025-10-17 15:47:09 +0900

    chore : 테스트  라우팅 세팅

--- src/testRoutes/index.tsx
+++ src/testRoutes/index.tsx
@@ -1,6 +1,7 @@
+import nariRouteArray from './_nariRouteArray'
 import thepottTestRouteArray from './_thepotTestRouteArray'
 import hyejeongTestRouteArray from './testPages/hyejeong/_hyejeongTestRouteArray'
 
-const testRouteArray = [...thepottTestRouteArray, ...hyejeongTestRouteArray]
+const testRouteArray = [...thepottTestRouteArray, ...hyejeongTestRouteArray, ...nariRouteArray]
 
 export default testRouteArray
---
+++ src/testRoutes/testPages/nari/_nariTastPage.tsx
@@ -0,0 +1,9 @@
+import RecruitCard from '@/components/recruit/RecruitCard' // 절대경로 없으면 상대경로로
+
+export default function NariTastPage() {
+  return (
+    <div className="p-6">
+      <RecruitCard />
+    </div>
+  )
+}
