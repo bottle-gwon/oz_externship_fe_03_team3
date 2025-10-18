8ceed437d68645ddcb38a32407260c68cd410424
Tynahan <134694901+ThePott@users.noreply.github.com>
2025-10-18 16:26:22 +0900

    feat: roundbox 모든 속성에 대한 test 구축 #34

--- src/testRoutes/testPages/thepott/ThePottRoundBoxPage.tsx
+++ src/testRoutes/testPages/thepott/ThePottRoundBoxPage.tsx
@@ -1,5 +1,6 @@
-import { Vstack } from '@/components/commonInGeneral/layout'
+import { Hstack, Vstack } from '@/components/commonInGeneral/layout'
 import RoundBox from '@/components/commonInGeneral/roundBox/RoundBox'
+import Text from '@/components/commonInGeneral/text/Text'
 
 const colorArray = [
   'mono-bright',
@@ -11,30 +12,67 @@ const colorArray = [
 ] as const
 type RoundBoxColor = (typeof colorArray)[number]
 
-const TestRoundBox = ({
-  color,
-  isBordered,
-}: {
-  color: RoundBoxColor
-  isBordered: boolean
-}) => {
+const paddingArray = ['none', 'xs', 'sm', 'md', 'lg', 'xl', 'xxl'] as const
+type RoundBoxPadding = (typeof paddingArray)[number]
+
+const radiusArray = ['sm', 'md', 'lg', 'full'] as const
+type RoundButtonRadius = (typeof radiusArray)[number]
+
+interface WithRoundBoxProps {
+  color?: RoundBoxColor
+  isShadowed?: boolean
+  isBordered?: boolean
+  padding?: RoundBoxPadding
+  radius?: RoundButtonRadius
+}
+
+const TestRoundBox = (props: WithRoundBoxProps) => {
+  return <RoundBox {...props}>{JSON.stringify(props)}</RoundBox>
+}
+
+const TestRoundBoxColumn = (props: WithRoundBoxProps) => {
   return (
-    <RoundBox
-      color={color}
-      isBordered={isBordered}
-      className="h-[100px] w-[100px]"
-    >
-      {color}__{isBordered}
-    </RoundBox>
+    <Vstack>
+      {colorArray.map((color) => (
+        <TestRoundBox key={color} {...props} color={color} />
+      ))}
+    </Vstack>
   )
 }
 
 const ThePottRoundBoxPage = () => {
   return (
     <Vstack>
-      {colorArray.map((color) => (
-        <TestRoundBox key={color} color={color} isBordered />
-      ))}
+      <Hstack>
+        <Text>is shadowed</Text>
+        {[true, false].map((isShadowed) => (
+          <TestRoundBoxColumn
+            key={Number(isShadowed)}
+            isShadowed={isShadowed}
+          />
+        ))}
+      </Hstack>
+      <Hstack>
+        <Text>is bordered</Text>
+        {[true, false].map((isBordered) => (
+          <TestRoundBoxColumn
+            key={Number(isBordered)}
+            isBordered={isBordered}
+          />
+        ))}
+      </Hstack>
+      <Hstack>
+        <Text>padding</Text>
+        {paddingArray.map((padding) => (
+          <TestRoundBoxColumn key={padding} padding={padding} />
+        ))}
+      </Hstack>
+      <Hstack>
+        <Text>radius</Text>
+        {radiusArray.map((radius) => (
+          <TestRoundBoxColumn key={radius} radius={radius} />
+        ))}
+      </Hstack>
     </Vstack>
   )
 }
