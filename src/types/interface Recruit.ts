363c114128436b910d1229f390a4d6ae98bcba32
nari-0v0 <ej9811@naver.com>
2025-10-19 17:09:17 +0900

    feat : 목록카드 프롭스 파일 추가 및 프롭스 분리 #14

---
+++ src/types/interface Recruit.ts
@@ -0,0 +1,16 @@
+export interface recruit {
+  id: number
+  title: string
+  content_preview: string
+  due_date: string
+  expected_personnel: number
+  current_personnel: number
+  status: 'open' | 'closed' | string
+  tags: string[]
+  study_group: { id: number; name: string } | null
+  bookmark_count: number
+  is_bookmarked: boolean
+  views: number
+  created_at: string
+  author: { id: number; nickname: string }
+}
