3c9f35acfc741c4552045a6d837b13c0e03ce25e
nari-0v0 <ej9811@naver.com>
2025-10-20 18:21:36 +0900

    feat: 목업파일 추가 #43

---
+++ src/testRoutes/testPages/nari/_TestMokSummary.ts
@@ -0,0 +1,28 @@
+import img from '@/testRoutes/testPages/nari/d8b6dce9691774374a170cfc1d2c92b1b571c146.jpg'
+import type { recruit } from '@/types/_interfaceSummary'
+
+export const mockSummaryCard: recruit[] = [
+  {
+    id: 301,
+    title: '웹 개발 스터디 모집',
+    thumbnail_url: img,
+    expected_personnel: 6,
+    current_personnel: 4,
+    status: 'open',
+    tags: [
+      { id: 1, name: 'AI' },
+      { id: 2, name: 'Python' },
+      { id: 3, name: '딥러닝' },
+    ],
+    lectures: [
+      {
+        name: '프론트엔드 기초',
+        instructor: '김강사',
+      },
+    ],
+    due_date: '2025-11-20',
+    bookmark_count: 3,
+    views: 125,
+    created_at: '2025-10-12T14:55:00Z',
+  },
+]
---
+++ src/types/_interfaceSummary.ts
@@ -0,0 +1,14 @@
+export interface recruit {
+  id: number
+  title: string
+  thumbnail_url: string
+  expected_personnel: number
+  current_personnel: number
+  status: string
+  tags: { id: number; name: string }[]
+  lectures: { name: string; instructor: string }[]
+  due_date: string
+  bookmark_count: number
+  views: number
+  created_at: string
+}
