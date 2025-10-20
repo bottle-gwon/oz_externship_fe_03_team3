b2730b19164732fe8fe84edb7ab49211ed084e5c
Tynahan <nusilite@gmail.com>
2025-10-20 12:55:06 +0900

    feat: 모든 화면에 배경색 적용 #45

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -5,7 +5,7 @@ import Header from '../../components/layout/header/Header'
 
 const Layout = () => {
   return (
-    <FullScreen>
+    <FullScreen className="bg-gray-50">
       <Header />
       <FlexOneContainer isYScrollable>
         <Outlet />
