9ad6351f567b0f02b226e4dc5b6d5a9e7a080b6b
nari-0v0 <ej9811@naver.com>
2025-10-21 15:00:55 +0900

    feat : 지원자모집 스켈레톤 제작 및 미리보기 연결 #66

--- src/testRoutes/testPages/nari/ApplicationModalPage.tsx
+++ src/testRoutes/testPages/nari/ApplicationModalPage.tsx
@@ -1,29 +1,71 @@
 import Button from '@/components/commonInGeneral/button/Button'
 import { Vstack } from '@/components/commonInGeneral/layout'
 import Modal from '@/components/commonInGeneral/modal/Modal'
-import Text from '@/components/commonInGeneral/text/Text'
+import { Send } from 'lucide-react'
 import { useState } from 'react'
 
-const ThePottModalPage = () => {
+const ApplicationModalPage = () => {
   const [isOn, setIsOn] = useState(false)
-  const longIndexArray = [...Object.keys(Array(50).fill(0))]
 
   return (
     <>
       <Modal isOn={isOn} onClose={() => setIsOn(false)}>
-        <Modal.Header>이게 헤더</Modal.Header>
-        <Modal.Body>이게 바디</Modal.Body>
-        <Modal.Footer>이게 푸터</Modal.Footer>
+        <Modal.Header>
+          <div>
+            <div>스터디 지원서 작성</div>
+            <div>지원서 타이틀</div>
+          </div>
+        </Modal.Header>
+        <Modal.Body>
+          <div>
+            <div>
+              <div>자기소개</div>
+              <div>입력 인풋</div>
+              <div>글자수</div>
+            </div>
+            <div>
+              <div>지원 동기</div>
+              <div>입력 인풋</div>
+              <div>글자수</div>
+            </div>
+            <div>
+              <div>스터디 목표</div>
+              <div>입력 인풋</div>
+              <div>글자수</div>
+            </div>
+            <div>
+              <div>가능한 시간대</div>
+              <div>입력 인풋</div>
+              <div>글자수</div>
+            </div>
+            <div>
+              <div>스터디 경험 유무</div>
+              <div>체크박스</div>
+              <div>스터디 참여 경험이 있습니다</div>
+            </div>
+            <div>
+              <div>구체적인 스터디 경험</div>
+              <div>입력 인풋</div>
+              <div>글자수</div>
+            </div>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <div>
+            <div>* 표시된 항목은 필수 입력 사항입니다.</div>
+            <button>취소</button>
+            <button>
+              <Send />
+              지원서 제출
+            </button>
+          </div>
+        </Modal.Footer>
       </Modal>
       <Vstack padding="xxl">
-        <Button onClick={() => setIsOn(true)}>누르면 모달 켜짐</Button>
-        {longIndexArray.map((index) => (
-          <Text key={index}>가나다라</Text>
-        ))}
         <Button onClick={() => setIsOn(true)}>누르면 모달 켜짐</Button>
       </Vstack>
     </>
   )
 }
 
-export default ThePottModalPage
+export default ApplicationModalPage
