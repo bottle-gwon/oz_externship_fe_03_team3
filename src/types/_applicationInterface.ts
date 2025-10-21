a7062921229cd51a778beba2bebc027b2b83a4f8
nari-0v0 <ej9811@naver.com>
2025-10-21 14:39:34 +0900

    feat : 더미데이터 추가 및 모달 스켈레톤 제작 #66

---
+++ src/types/_applicationInterface.ts
@@ -0,0 +1,8 @@
+export interface Application {
+  self_introduction: string
+  motivation: string
+  objective: string
+  available_time: string
+  has_study_experience: boolean
+  study_experience: string
+}
