44d5eb885a6daaa0197aec23b1bd34e1f0996c4a
Tynahan <nusilite@gmail.com>
2025-10-22 15:52:21 +0900

    feat: 작성 페이지 외곽 레이아웃 설정 #87

--- src/pages/recruit/write/RecruitWritePage.tsx
+++ src/pages/recruit/write/RecruitWritePage.tsx
@@ -1,5 +1,23 @@
+import { Vstack } from '@/components/commonInGeneral/layout'
+import Container from '@/components/commonInGeneral/layout/_Container'
+import RoundBox from '@/components/commonInGeneral/roundBox/RoundBox'
+import TitleSection from '@/components/titleSection/TitleSection'
+
 const RecruitWritePage = () => {
-  return <div></div>
+  return (
+    <Container width="lg" isPadded>
+      <Vstack gap="xxl">
+        {/* 여기 바텀 패딩이 없어야할 거 같은데 */}
+        {/* isPaddedBottom 옵션 넣어달라고 하고 우선 이대로 진행하자 */}
+        <TitleSection type="create" />
+        <RoundBox></RoundBox>
+        <RoundBox></RoundBox>
+        <RoundBox></RoundBox>
+        <RoundBox></RoundBox>
+        <RoundBox></RoundBox>
+      </Vstack>
+    </Container>
+  )
 }
 
 export default RecruitWritePage
