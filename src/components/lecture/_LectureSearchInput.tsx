bd78a87c304a2c3091a3429f0da92962a4fd6225
Tynahan <nusilite@gmail.com>
2025-10-22 14:07:12 +0900

    feat: 강의 검색 인풋 제작 #83

--- src/components/lecture/_LectureSearchInput.tsx
+++ src/components/lecture/_LectureSearchInput.tsx
@@ -1,11 +1,35 @@
 import { Search } from 'lucide-react'
 import Input from '../commonInGeneral/inputFamily/input/Input'
+import { useEffect, useState } from 'react'
+import useDebounce from '@/hooks/useDebounce'
+
+// TODO: 이건 api 연결하면서 삭제해야 함!
+const dummySearchApi = () => {}
 
 const LectureSearchInput = () => {
+  const [searchText, setSearchText] = useState('')
+  const [devounceValue, cancel] = useDebounce(searchText, 500)
+
+  useEffect(() => {}, [devounceValue])
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== 'Enter') {
+      return
+    }
+
+    cancel()
+  }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value)
+  }
+
   return (
     <Input
       icon={<Search size={14} />}
       placeholder="강의명이나 강사명으로 검색..."
+      onKeyDown={handleKeyDown}
+      onChange={handleChange}
     />
   )
 }
