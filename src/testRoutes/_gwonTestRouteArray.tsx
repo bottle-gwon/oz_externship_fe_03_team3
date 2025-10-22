b0614c8920f87e732363fdabd25dba83053aea59
Tynahan <nusilite@gmail.com>
2025-10-22 15:24:46 +0900

    feat: 테스트 라우트 코드 스플리팅 적용 #85

--- src/testRoutes/_gwonTestRouteArray.tsx
+++ src/testRoutes/_gwonTestRouteArray.tsx
@@ -1,5 +1,8 @@
-import GwonTagModalTest from './testPages/gwon/GwonTagModalTest'
-import GwonTestPage from './testPages/gwon/GwonTestPage'
+import { lazy } from 'react'
+
+// 테스트 라우트에서도 코드 스플리팅을 하려고 합니다!
+const GwonTagModalTest = lazy(() => import('./testPages/gwon/GwonTagModalTest'))
+const GwonTestPage = lazy(() => import('./testPages/gwon/GwonTestPage'))
 
 const gwonTestRouteArray = [
   { path: '/test/gwon', element: <GwonTestPage /> },
--- src/testRoutes/_nariRouteArray.tsx
+++ src/testRoutes/_nariRouteArray.tsx
@@ -1,4 +1,7 @@
-import NariTastPage from './testPages/nari/_nariTastPage'
+import { lazy } from 'react'
+
+// 테스트 라우트에서도 코드 스플리팅을 하려고 합니다!
+const NariTastPage = lazy(() => import('./testPages/nari/_nariTastPage'))
 
 const nariRouteArray = [{ path: '/test/nari', element: <NariTastPage /> }]
 
