32e8df18c34e1167338163dc867766049ef78949
Tynahan <nusilite@gmail.com>
2025-10-23 11:42:03 +0900

    feat: 로고 삽입 #54

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,13 +1,16 @@
 import Button from '@/components/commonInGeneral/button/Button'
 import { Hstack } from '@/components/commonInGeneral/layout'
 import Container from '@/components/commonInGeneral/layout/_Container'
+import Logo from '@/assets/logo.svg'
 
 const Header = () => {
   return (
     <div className="border border-b-gray-200 bg-white">
       <Container width="lg" className="px-oz-xxl py-oz-sm">
         <Hstack>
-          <h1 className="grow">LOGO PLACEHOLDER</h1>
+          <div className="grow">
+            <img src={Logo} />
+          </div>
           <Button variant="ghost">강의 목록</Button>
           <Button variant="ghost">스터디 그룹</Button>
           <Button variant="ghost">구인 공고</Button>
