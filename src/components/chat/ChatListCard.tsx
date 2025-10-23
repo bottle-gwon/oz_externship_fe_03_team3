a6ec3578d5802d8df87021212b53daca96a32936
bottle-gwon <lazyguy1@daum.net>
2025-10-23 17:32:40 +0900

    feat: 채팅 목록 데모 제작 #89

---
+++ src/components/chat/ChatListCard.tsx
@@ -0,0 +1,48 @@
+import { Hstack, Vstack } from '../commonInGeneral/layout'
+
+// Todo 관련 API 나오면 고치기
+// Note 관련 API가 없는 관계로 UI확인용으로 임의로 작성 했습니다.
+type ChatListCardType = {
+  title: string
+  study_group_id: number
+  sender_id: number
+  sender_nickname: string
+  created_at: string
+  new_message: number
+  content: string | null
+}
+
+interface ChatListCardInterface {
+  room: ChatListCardType
+}
+
+const ChatListCard = ({ room }: ChatListCardInterface) => {
+  const date = new Date(room.created_at)
+  const month = date.getMonth()
+  const day = date.getDay()
+  return (
+    <Vstack
+      gap="none"
+      className="min-h-[76px] w-[318px] cursor-pointer border-b border-gray-200 p-3"
+    >
+      <Hstack className="justify-between">
+        <p className="text-sm text-gray-900">{room.title}</p>
+        <Hstack>
+          <p className="text-xs text-gray-500">{`${month}월 ${day}일`}</p>
+          <p className="bg-danger-500 h-[20px] min-w-[20px] rounded-full px-[6px] py-[2px] text-center text-xs text-white">
+            {room.new_message >= 1 ? room.new_message : null}
+          </p>
+        </Hstack>
+      </Hstack>
+      <Hstack>
+        <p className="text-xs text-gray-600">
+          {room.content
+            ? `${room.sender_nickname}: ${room.content}`
+            : '(대화가 없습니다. 대화를 시작해보세요)'}
+        </p>
+      </Hstack>
+    </Vstack>
+  )
+}
+
+export default ChatListCard
---
+++ src/types/_chatInterfaces.ts
@@ -0,0 +1,10 @@
+export interface messageList {
+  id: number
+  sender_id: number
+  sender_nickname: string
+  study_group_id: number
+  content: string
+  file_url: string | null
+  is_read: boolean
+  created_at: string
+}
