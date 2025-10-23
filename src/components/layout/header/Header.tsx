2237eddcfb17789a7ed1983abb2d78c153a9f6d6
Tynahan <nusilite@gmail.com>
2025-10-23 11:29:29 +0900

    feat: 헤더 레이아웃 설정 #54

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,5 +1,22 @@
+import Button from '@/components/commonInGeneral/button/Button'
+import { Hstack } from '@/components/commonInGeneral/layout'
+import Container from '@/components/commonInGeneral/layout/_Container'
+
 const Header = () => {
-  return <div>Header 제작해야 합니다</div>
+  return (
+    <div className="border border-b-gray-200 bg-white">
+      <Container width="lg" className="px-oz-xxl py-oz-sm">
+        <Hstack>
+          <h1 className="grow">LOGO PLACEHOLDER</h1>
+          <Button variant="ghost">강의 목록</Button>
+          <Button variant="ghost">스터디 그룹</Button>
+          <Button variant="ghost">구인 공고</Button>
+          <Button variant="ghost">로그인</Button>
+          <Button color="primary">회원가입</Button>
+        </Hstack>
+      </Container>
+    </div>
+  )
 }
 
 export default Header
