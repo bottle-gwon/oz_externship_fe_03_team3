c724a9972a66d3fd27abe88144fb4dea11d90286
nari-0v0 <ej9811@naver.com>
2025-10-23 19:02:43 +0900

    refactor : h,vstack 반영 (summary카드) #99

--- src/components/recruit/manage/RecruitSummaryCard.tsx
+++ src/components/recruit/manage/RecruitSummaryCard.tsx
@@ -1,3 +1,4 @@
+import { Hstack, Vstack } from '@/components/commonInGeneral/layout'
 import RoundBox from '@/components/commonInGeneral/roundBox/RoundBox'
 import type { Recruit } from '@/types'
 
@@ -14,7 +15,7 @@ const RecruitSummaryCard = ({ myRecruitArray }: SummaryProps) => {
     (item) => item.is_closed === true
   ).length
   return (
-    <div className="flex flex-wrap items-start justify-start gap-5">
+    <Hstack gap="xl" padding="xs" className="items-start">
       <RoundBox
         color="mono-bright"
         isBordered
@@ -24,7 +25,7 @@ const RecruitSummaryCard = ({ myRecruitArray }: SummaryProps) => {
         radius="sm"
         className="h-20 w-62"
       >
-        <div className="flex-start flex items-center gap-4">
+        <Hstack gap="sm" padding="xs" className="items-center">
           <RoundBox
             color="mono-dim"
             isBordered={false}
@@ -34,16 +35,16 @@ const RecruitSummaryCard = ({ myRecruitArray }: SummaryProps) => {
             radius="md"
             className="h-10 w-10"
           >
-            <FileText className="flex size-4" />
+            <FileText className="size-4" />
             {/* 추후 svg 아이콘으로 추가 */}
           </RoundBox>
-          <div className="flex h-11 flex-col justify-center">
+          <Vstack gap="xs" padding="xs" className="h-11 justify-center">
             <span className="flex items-center truncate text-xl font-bold text-gray-900">
               {totalCount}
             </span>
             <span className="text-xs text-gray-600">전체</span>
-          </div>
-        </div>
+          </Vstack>
+        </Hstack>
       </RoundBox>
 
       <RoundBox
@@ -55,7 +56,7 @@ const RecruitSummaryCard = ({ myRecruitArray }: SummaryProps) => {
         radius="sm"
         className="h-20 w-62"
       >
-        <div className="flex-start flex items-center gap-4">
+        <Hstack gap="sm" padding="xs" className="items-center">
           <RoundBox
             color="success"
             isBordered={false}
@@ -65,16 +66,16 @@ const RecruitSummaryCard = ({ myRecruitArray }: SummaryProps) => {
             radius="md"
             className="h-10 w-10"
           >
-            <Megaphone className="flex size-4" />
+            <Megaphone className="size-4" />
             {/* 추후 svg 아이콘으로 추가 */}
           </RoundBox>
-          <div className="flex h-11 flex-col justify-center">
+          <Vstack gap="xs" padding="xs" className="h-11 justify-center">
             <span className="flex items-center truncate text-xl font-bold text-gray-900">
               {openCount}
             </span>
             <span className="text-xs text-gray-600">모집중</span>
-          </div>
-        </div>
+          </Vstack>
+        </Hstack>
       </RoundBox>
 
       <RoundBox
@@ -86,7 +87,7 @@ const RecruitSummaryCard = ({ myRecruitArray }: SummaryProps) => {
         radius="sm"
         className="h-20 w-62"
       >
-        <div className="flex-start flex items-center gap-4">
+        <Hstack gap="sm" padding="xs" className="items-center">
           <RoundBox
             color="danger"
             isBordered={false}
@@ -96,17 +97,17 @@ const RecruitSummaryCard = ({ myRecruitArray }: SummaryProps) => {
             radius="md"
             className="h-10 w-10"
           >
-            <Clock3 className="flex size-4" />
+            <Clock3 className="size-4" />
           </RoundBox>
-          <div className="flex h-11 flex-col justify-center">
+          <Vstack gap="xs" padding="xs" className="h-11 justify-center">
             <span className="flex items-center truncate text-xl font-bold text-gray-900">
               {closedCount}
             </span>
             <span className="text-xs text-gray-600">마감됨</span>
-          </div>
-        </div>
+          </Vstack>
+        </Hstack>
       </RoundBox>
-    </div>
+    </Hstack>
   )
 }
 
