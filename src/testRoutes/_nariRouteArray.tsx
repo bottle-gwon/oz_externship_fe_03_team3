05776e5a34daa5bd2df021dae0ae2a0730f0b6c4
nari-0v0 <ej9811@naver.com>
2025-10-24 17:52:02 +0900

    refactor : ui제작 완료 #95

--- src/testRoutes/_nariRouteArray.tsx
+++ src/testRoutes/_nariRouteArray.tsx
@@ -1,5 +1,6 @@
 import { lazy } from 'react'
-import AnnouncementManagementPage from './testPages/nari/AnnouncementManagementPage'
+import RecruitManagementPage from './testPages/nari/RecruitManagementPage'
+import HowToUseSelect from '@/components/commonInGeneral/select/HowToUseSelect'
 
 // 테스트 라우트에서도 코드 스플리팅을 하려고 합니다!
 const NariTastPage = lazy(() => import('./testPages/nari/_nariTastPage'))
@@ -10,7 +11,8 @@ const ApplicationModalPage = lazy(
 const nariRouteArray = [
   { path: '/test/nari', element: <NariTastPage /> },
   { path: '/test/narimodal', element: <ApplicationModalPage /> },
-  { path: '/test/narimanage', element: <AnnouncementManagementPage /> },
+  { path: '/test/narimanage', element: <RecruitManagementPage /> },
+  { path: '/test/narimanage1', element: <HowToUseSelect /> },
 ]
 
 export default nariRouteArray
---
+++ src/testRoutes/testPages/nari/RecruitManagementPage.tsx
@@ -0,0 +1,62 @@
+import RecruitCard from '@/components/recruit/recruitCard/RecruitCard'
+import Header from '@/components/layout/header/Header'
+import TitleSection from '@/components/titleSection/TitleSection'
+import RoundBox from '@/components/commonInGeneral/roundBox/RoundBox'
+import { mockRecruits } from './_TestMokData'
+import RecruitSummaryCard from '@/components/recruit/manage/RecruitSummaryCard'
+import { Hstack, Vstack } from '@/components/commonInGeneral/layout'
+import Container from '@/components/commonInGeneral/layout/_Container'
+import Select from '@/components/commonInGeneral/select/Select'
+import Text from '@/components/commonInGeneral/text/Text'
+import { useState } from 'react'
+
+const RecruitManagementPage = () => {
+  const [selectedOption, setSelectedOption] = useState<string | null>(null)
+  return (
+    <Vstack className="bg-[#F9FAFB]">
+      <Header />
+      <Container className="py-oz-xxl flex flex-col">
+        <TitleSection type="manage" />
+        <Vstack gap="xl">
+          <RecruitSummaryCard myRecruitArray={mockRecruits} />
+
+          <RoundBox isShadowed={false}>
+            <Hstack>
+              <Vstack className="w-full">
+                <Text>상태</Text>
+                <Select onOptionSelect={setSelectedOption}>
+                  <Select.Trigger>{`전체 (${mockRecruits.length})`}</Select.Trigger>
+                  <Select.Content>
+                    <Select.Option>{`전체 (${mockRecruits.length})`}</Select.Option>
+                    <Select.Option>{`모집중 (${mockRecruits.filter((item) => !item.is_closed).length})`}</Select.Option>
+                    <Select.Option>{`마감됨 (${mockRecruits.filter((item) => item.is_closed).length})`}</Select.Option>
+                  </Select.Content>
+                </Select>
+              </Vstack>
+              <Vstack className="w-full">
+                <Text>정렬</Text>
+                <Select onOptionSelect={setSelectedOption}>
+                  <Select.Trigger>최신순</Select.Trigger>
+                  <Select.Content>
+                    <Select.Option>최신순</Select.Option>
+                    <Select.Option>북마크 순</Select.Option>
+                    <Select.Option>조회수 순</Select.Option>
+                  </Select.Content>
+                </Select>
+              </Vstack>
+            </Hstack>
+          </RoundBox>
+
+          <Vstack gap="lg">
+            <h1>내 공고 목록 ({mockRecruits.length})</h1>
+            {mockRecruits.map((recruit) => (
+              <RecruitCard isMine key={recruit.id} recruit={recruit} />
+            ))}
+          </Vstack>
+        </Vstack>
+      </Container>
+    </Vstack>
+  )
+}
+
+export default RecruitManagementPage
