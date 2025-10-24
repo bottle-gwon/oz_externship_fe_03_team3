31575006df145e8a45f650ed5dfb0ce64f16bc59
nari-0v0 <ej9811@naver.com>
2025-10-24 15:01:15 +0900

    refactor : 컴포넌트 적용 및 더미데이터 목록추가  #95

--- src/testRoutes/testPages/nari/AnnouncementManagementPage.tsx
+++ src/testRoutes/testPages/nari/AnnouncementManagementPage.tsx
@@ -1,63 +1,46 @@
 import { useState } from 'react'
 import Select from '@/components/commonInGeneral/select/Select'
 import RecruitCard from '@/components/recruit/recruitCard/RecruitCard'
-import type { Recruit, MyRecruit } from '@/types'
+import type { LectureOrderingInText, Recruit } from '@/types'
 import Header from '@/components/layout/header/Header'
 import TitleSection from '@/components/titleSection/TitleSection'
 import RoundBox from '@/components/commonInGeneral/roundBox/RoundBox'
-import RecruitSummaryCard from '@/components/recruit/manage/_recruitSummaryCard'
 import { mockRecruits } from './_TestMokData'
-import { mockSummaryCard } from './_TestMokSummary'
 import Text from '@/components/commonInGeneral/text/Text'
+import RecruitSummaryCard from '@/components/recruit/manage/RecruitSummaryCard'
+import { GridContainer, Vstack } from '@/components/commonInGeneral/layout'
+import LectureCategorySelect from '@/components/lecture/_LectureCategorySelect'
+import LectureOrderingSelect from '@/components/lecture/_LectureOrderingSelect'
+import Container from '@/components/commonInGeneral/layout/_Container'
 
-export type SummaryProps = { myRecruitArray: MyRecruit[] }
-
-export type RecruitCardProps = {
-  isMine?: boolean
-  cardClassName?: string
-  imageClassName?: string
-}
-
-const AnnouncementManagementPage = () => {
+const AnnouncementManagementPage = (isMine = false) => {
   const [selectedOption, setSelectedOption] = useState<string | null>(null)
+  const [selectedOrderingInText, setSelectedOrderingInText] =
+    useState<LectureOrderingInText>('최신순')
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null)
 
   return (
-    <div>
+    <Vstack className="bg-[#F9FAFB]">
       <Header />
-      <TitleSection type="manage" />
+      <Container className="py-oz-xxl flex flex-col bg-[#F9FAFB]">
+        <TitleSection type="manage" />
+        <Vstack gap="xl" padding="">
+          <RecruitSummaryCard myRecruitArray={mockRecruits} />
 
-      <RoundBox
-        color="mono-bright"
-        isBordered
-        borderStyle="solid"
-        isShadowed
-        padding="md"
-        radius="md"
-      >
-        <Text>{JSON.stringify({ selectedOption })}</Text>
-        <Select onOptionSelect={(option) => setSelectedOption(option)}>
-          <Select.Trigger>이걸 누르면 열립니다</Select.Trigger>
-          <Select.Content>
-            <Select.Option>전체</Select.Option>
-            <Select.Option>모집중</Select.Option>
-            <Select.Option>마감됨</Select.Option>
-          </Select.Content>
-        </Select>
-        <Text>{JSON.stringify({ selectedOption })}</Text>
-        <Select onOptionSelect={(option) => setSelectedOption(option)}>
-          <Select.Trigger>이걸 누르면 열립니다</Select.Trigger>
-          <Select.Content>
-            <Select.Option>최신순</Select.Option>
-            <Select.Option>오래된 순</Select.Option>
-            <Select.Option>조회수 순</Select.Option>
-            <Select.Option>북마크 순</Select.Option>
-          </Select.Content>
-        </Select>
-      </RoundBox>
-      <h1>내 공고 목록 ()</h1>
-      <RecruitSummaryCard />
-      <RecruitCard />
-    </div>
+          <RoundBox isShadowed={false}>
+            <GridContainer />
+            <LectureCategorySelect setSelectedCategory={setSelectedCategory} />
+            <LectureOrderingSelect
+              setSelectedOrderingInText={setSelectedOrderingInText}
+            />
+          </RoundBox>
+          <h1>내 공고 목록 ({mockRecruits.length})</h1>
+        </Vstack>
+        {mockRecruits.map((recruit) => (
+          <RecruitCard isMine={true} key={recruit.id} recruit={recruit} />
+        ))}
+      </Container>
+    </Vstack>
   )
 }
 
