05d90d17c46e97cc4762ac4e05e130cf4a286f29
Tynahan <nusilite@gmail.com>
2025-10-27 05:33:32 +0900

    style: 드롭존 아이템 글씨 크기 및 갭 조절 #117

--- src/components/commonInGeneral/fileDropzone/_FileDropzoneUploadedItem.tsx
+++ src/components/commonInGeneral/fileDropzone/_FileDropzoneUploadedItem.tsx
@@ -1,8 +1,9 @@
-import { X } from 'lucide-react'
+import { FileText, X } from 'lucide-react'
 import { Hstack } from '../layout'
 import RoundBox from '../roundBox/RoundBox'
 import useFileDropzoneContext from './_useFileDropzoneContext'
 import { memo } from 'react'
+import Button from '../button/Button'
 
 interface FifleDropzoneUploaedItemProps {
   fileId: number
@@ -22,9 +23,14 @@ const FileDropzoneUploadedItem = memo(
 
     return (
       <RoundBox>
-        <Hstack className="justify-between">
-          {file.name}
-          <X onClick={handleXClick} />
+        <Hstack className="items-center">
+          <Hstack className="gap-oz-sm grow items-center text-sm">
+            <FileText size={14} className="text-gray-400" />
+            {file.name}
+          </Hstack>
+          <Button variant="ghost" className="p-0">
+            <X onClick={handleXClick} size={14} />
+          </Button>
         </Hstack>
       </RoundBox>
     )
