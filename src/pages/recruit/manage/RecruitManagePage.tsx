ae266d9025343063d8c76739e76f813a39aa2152
nari-0v0 <ej9811@naver.com>
2025-10-26 20:26:04 +0900

    refactor : 헤더제거, isPadded 속성 추가, 타이틀섹션 컴포넌트교체, 채팅컴포넌트추가 #95

--- src/pages/recruit/manage/RecruitManagePage.tsx
+++ src/pages/recruit/manage/RecruitManagePage.tsx
@@ -1,6 +1,4 @@
 import RecruitCard from '@/components/recruit/recruitCard/RecruitCard'
-import Header from '@/components/layout/header/Header'
-import TitleSection from '@/components/titleSection/TitleSection'
 import RoundBox from '@/components/commonInGeneral/roundBox/RoundBox'
 import { mockRecruits } from '../../../testRoutes/testPages/nari/_TestMokData'
 import RecruitSummaryCard from '@/components/recruit/manage/RecruitSummaryCard'
@@ -8,52 +6,77 @@ import { Hstack, Vstack } from '@/components/commonInGeneral/layout'
 import Container from '@/components/commonInGeneral/layout/_Container'
 import Select from '@/components/commonInGeneral/select/Select'
 import Text from '@/components/commonInGeneral/text/Text'
+import SubHeader from '@/components/commonInProject/SubHeader/SubHeader'
+import SubHeaderTitleSection from '@/components/commonInProject/SubHeader/_SubHeaderTtileSectoin'
+import Button from '@/components/commonInGeneral/button/Button'
+import SubHeaderButtonSection from '@/components/commonInProject/SubHeader/_SubHeaderButtonSection'
+import FloatingButtonContainer from '@/components/layout/floatingButton/FloatingButtonContainer'
+import FloatingButton from '@/components/layout/floatingButton/FloatingButton'
+import { MessageCircle } from 'lucide-react'
 
 const RecruitManagementPage = () => {
+  const badgeTest = 12
   return (
-    <Vstack className="bg-[#F9FAFB]">
-      <Header />
-      <Container className="py-oz-xxl flex flex-col">
-        <TitleSection type="manage" />
-        <Vstack gap="xl">
-          <RecruitSummaryCard myRecruitArray={mockRecruits} />
+    <Container isPadded className="py-oz-xxl bg-[#F9FAFB]">
+      <SubHeader isBackButtonVisible>
+        <SubHeaderTitleSection>
+          <SubHeader.Title>공고 관리</SubHeader.Title>
+          <SubHeader.Subtitle>
+            내가 등록한 스터디 구인공고를 관리하세요
+          </SubHeader.Subtitle>
+        </SubHeaderTitleSection>
+        <SubHeaderButtonSection>
+          <Button color="primary" variant="contained" size="lg">
+            + 새 공고 작성하기
+          </Button>
+        </SubHeaderButtonSection>
+      </SubHeader>
 
-          <RoundBox isShadowed={false}>
-            <Hstack>
-              <Vstack className="w-full">
-                <Text>상태</Text>
-                <Select onOptionSelect={() => null}>
-                  <Select.Trigger>{`전체 (${mockRecruits.length})`}</Select.Trigger>
-                  <Select.Content>
-                    <Select.Option>{`전체 (${mockRecruits.length})`}</Select.Option>
-                    <Select.Option>{`모집중 (${mockRecruits.filter((item) => !item.is_closed).length})`}</Select.Option>
-                    <Select.Option>{`마감됨 (${mockRecruits.filter((item) => item.is_closed).length})`}</Select.Option>
-                  </Select.Content>
-                </Select>
-              </Vstack>
-              <Vstack className="w-full">
-                <Text>정렬</Text>
-                <Select onOptionSelect={() => null}>
-                  <Select.Trigger>최신순</Select.Trigger>
-                  <Select.Content>
-                    <Select.Option>최신순</Select.Option>
-                    <Select.Option>북마크 순</Select.Option>
-                    <Select.Option>조회수 순</Select.Option>
-                  </Select.Content>
-                </Select>
-              </Vstack>
-            </Hstack>
-          </RoundBox>
+      <Vstack gap="xl">
+        <RecruitSummaryCard myRecruitArray={mockRecruits} />
 
-          <Vstack gap="none">
-            <h1>내 공고 목록 ({mockRecruits.length})</h1>
-            {mockRecruits.map((recruit) => (
-              <RecruitCard isMine key={recruit.id} recruit={recruit} />
-            ))}
-          </Vstack>
+        <RoundBox isShadowed={false}>
+          <Hstack>
+            <Vstack className="w-full">
+              <Text>상태</Text>
+              <Select onOptionSelect={() => null}>
+                <Select.Trigger>{`전체 (${mockRecruits.length})`}</Select.Trigger>
+                <Select.Content>
+                  <Select.Option>{`전체 (${mockRecruits.length})`}</Select.Option>
+                  <Select.Option>{`모집중 (${mockRecruits.filter((item) => !item.is_closed).length})`}</Select.Option>
+                  <Select.Option>{`마감됨 (${mockRecruits.filter((item) => item.is_closed).length})`}</Select.Option>
+                </Select.Content>
+              </Select>
+            </Vstack>
+            <Vstack className="w-full">
+              <Text>정렬</Text>
+              <Select onOptionSelect={() => null}>
+                <Select.Trigger>최신순</Select.Trigger>
+                <Select.Content>
+                  <Select.Option>최신순</Select.Option>
+                  <Select.Option>북마크 순</Select.Option>
+                  <Select.Option>조회수 순</Select.Option>
+                </Select.Content>
+              </Select>
+            </Vstack>
+          </Hstack>
+        </RoundBox>
+
+        <Vstack gap="none">
+          <h1>내 공고 목록 ({mockRecruits.length})</h1>
+          {mockRecruits.map((recruit) => (
+            <RecruitCard isMine key={recruit.id} recruit={recruit} />
+          ))}
         </Vstack>
-      </Container>
-    </Vstack>
+      </Vstack>
+
+      <FloatingButtonContainer>
+        {/* 메시지 아이콘 */}
+        <FloatingButton theme="primary" badge={badgeTest}>
+          <MessageCircle size={24} />
+        </FloatingButton>
+      </FloatingButtonContainer>
+    </Container>
   )
 }
 
