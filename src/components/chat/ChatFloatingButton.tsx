a035510cd38b1dfe102900c193284847f829a8fd
bottle-gwon <lazyguy1@daum.net>
2025-10-27 14:31:00 +0900

    feat: 채팅 아이콘 로그인 시에만 출력 #132

--- src/components/chat/ChatFloatingButton.tsx
+++ src/components/chat/ChatFloatingButton.tsx
@@ -2,11 +2,22 @@ import useStudyHubStore from '@/store/store'
 import FloatingButton from '../layout/floatingButton/FloatingButton'
 import { MessageCircle, X } from 'lucide-react'
 
+const ChatIcon = (isOff: boolean) => {
+  if (isOff) {
+    return <MessageCircle size={24} />
+  } else {
+    return <X size={24} />
+  }
+}
+
 const ChatFloatingButton = () => {
   const badgeTest = 12 // 안 읽은 메시지
   const chatState = useStudyHubStore((state) => state.chatState)
   const openChatList = useStudyHubStore((state) => state.openChatList)
   const closeChatUI = useStudyHubStore((state) => state.closeChatUI)
+  const accessToken = useStudyHubStore((state) => state.accessToken)
+
+  const isOff = chatState.status === 'off'
 
   const onClickChat = () => {
     if (chatState.status === 'off') {
@@ -16,13 +27,13 @@ const ChatFloatingButton = () => {
     }
   }
 
+  if (!accessToken) {
+    return null
+  }
+
   return (
     <FloatingButton theme="primary" badge={badgeTest} onClick={onClickChat}>
-      {chatState.status === 'off' ? (
-        <MessageCircle size={24} />
-      ) : (
-        <X size={24} />
-      )}
+      {ChatIcon(isOff)}
     </FloatingButton>
   )
 }
