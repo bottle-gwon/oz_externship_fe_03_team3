d7b56ca0c8be2e4599e1042fb0b583ecc6058309
Tynahan <nusilite@gmail.com>
2025-10-27 14:58:19 +0900

    style: 툴바 스타일링 #127

--- src/components/commonInGeneral/markdownEditor/MarkdownEditor.tsx
+++ src/components/commonInGeneral/markdownEditor/MarkdownEditor.tsx
@@ -1,9 +1,11 @@
+import './markdown.css'
 import MDEditor from '@uiw/react-md-editor'
-import { Hstack, Vstack } from '../layout'
+import { Vstack } from '../layout'
 import RoundBox from '../roundBox/RoundBox'
 import { useState } from 'react'
 import rehypeSanitize from 'rehype-sanitize'
 import { commandArray, extraCommandArray } from './_commandArray'
+import markdownPlaceholder from './_markdownPlaceholder'
 
 const MarkdownEditor = () => {
   const [value, setValue] = useState<string | undefined>('')
@@ -17,7 +19,6 @@ const MarkdownEditor = () => {
         className="overflow-hidden"
         data-color-mode="light"
       >
-        <Hstack></Hstack>
         <Vstack className="gap-0">
           <MDEditor
             value={value}
@@ -30,6 +31,27 @@ const MarkdownEditor = () => {
             preview="edit"
             commands={commandArray}
             extraCommands={extraCommandArray}
+            textareaProps={{
+              placeholder: markdownPlaceholder,
+            }}
+            components={{
+              toolbar: (command, disabled, executeCommand) => {
+                if (command.keyCommand === 'code') {
+                  return (
+                    <button
+                      aria-label="Insert code"
+                      disabled={disabled}
+                      onClick={(evn) => {
+                        evn.stopPropagation()
+                        executeCommand(command, command.groupName)
+                      }}
+                    >
+                      Code
+                    </button>
+                  )
+                }
+              },
+            }}
           />
           <div className="bg-gray-50">
             <div>마크다운 문법을 사용할 수 있습니다.</div>
--- src/components/commonInGeneral/markdownEditor/_commandArray.tsx
+++ src/components/commonInGeneral/markdownEditor/_commandArray.tsx
@@ -1,30 +1,24 @@
 import { commands } from '@uiw/react-md-editor'
-import Button from '@/components/commonInGeneral/button/Button'
-import { Bold } from 'lucide-react'
+import { Bold, Code, Heading1, Italic, Link, List } from 'lucide-react'
 
 const customCodeEdit = { ...commands.codeEdit }
-customCodeEdit.icon = <Button>작성</Button>
+customCodeEdit.icon = <p>작성</p>
 const customCodePreivew = { ...commands.codePreview }
-customCodePreivew.icon = <Button>미리보기</Button>
+customCodePreivew.icon = <p>미리보기</p>
 export const commandArray = [customCodeEdit, customCodePreivew]
 
 const customBold = { ...commands.bold }
-customBold.icon = (
-  <Button>
-    <Bold />
-  </Button>
-)
-
+customBold.icon = <Bold size={16} />
 const customItalic = { ...commands.italic }
-customItalic.icon = <Button>I</Button>
+customItalic.icon = <Italic size={16} />
 const customCodeBlock = { ...commands.codeBlock }
-customCodeBlock.icon = <Button>CodeBlock</Button>
+customCodeBlock.icon = <Code size={16} />
 const customLink = { ...commands.link }
-customLink.icon = <Button>Link</Button>
+customLink.icon = <Link size={16} />
 const customHeading1 = { ...commands.heading1 }
-customHeading1.icon = <Button>Heading1</Button>
+customHeading1.icon = <Heading1 size={16} />
 const customUnorderedList = { ...commands.unorderedListCommand }
-customUnorderedList.icon = <Button>UnorderedList</Button>
+customUnorderedList.icon = <List size={16} />
 
 export const extraCommandArray = [
   customBold,
