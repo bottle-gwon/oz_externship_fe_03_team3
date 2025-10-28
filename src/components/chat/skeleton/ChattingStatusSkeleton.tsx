39f56cc60c25c2b76ba3fe51b1c5a97cc0386648
bottle-gwon <lazyguy1@daum.net>
2025-10-28 17:33:04 +0900

    feat: 채팅방 스켈레톤 추가 #141

---
+++ src/components/chat/skeleton/ChattingStatusSkeleton.tsx
@@ -0,0 +1,14 @@
+import Skeleton from '@/components/commonInGeneral/skeleton/Skeleton'
+
+const ChattingStatusSkeleton = () => {
+  return (
+    <Skeleton
+      radius="lg"
+      widthInPixel={302}
+      heightInPixel={24}
+      className="shrink-0"
+    />
+  )
+}
+
+export default ChattingStatusSkeleton
