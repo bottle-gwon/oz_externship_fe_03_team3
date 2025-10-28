700a0108998cf40fb0378f68d9b46dc178ce585d
bottle-gwon <lazyguy1@daum.net>
2025-10-29 02:15:26 +0900

    fix: 채팅창 스켈레톤 초기 로딩시 스크롤 막기 #141

--- src/components/layout/chattingRoom/ChattingStatus.tsx
+++ src/components/layout/chattingRoom/ChattingStatus.tsx
@@ -3,14 +3,17 @@ import type { ReactNode } from 'react'
 
 interface ChattingStatus {
   children: ReactNode
+  isPending: boolean
 }
 
 // 채팅 접속 상태 -> 아직 어떻게 전달 받을지 확정 나지 않아 props로 받음
-const ChattingStatus = ({ children }: ChattingStatus) => {
+const ChattingStatus = ({ children, isPending }: ChattingStatus) => {
+  const overflow = isPending ? 'overflow-hidden' : 'overflow-x-scroll'
+
   return (
     <Hstack
       gap="sm"
-      className="min-h-[41px] w-full items-center overflow-x-scroll border-b border-gray-200 bg-gray-50 p-2"
+      className={`${overflow} min-h-[41px] w-full items-center border-b border-gray-200 bg-gray-50 p-2`}
     >
       {children}
     </Hstack>
