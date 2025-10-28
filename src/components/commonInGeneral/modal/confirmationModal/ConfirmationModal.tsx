2ca07a0dc2187cca17af1db83480a4c4d8271819
Tynahan <nusilite@gmail.com>
2025-10-28 15:51:40 +0900

    feat: 확인 모달 합성 컴포넌트로 제작 #144

--- src/components/commonInGeneral/modal/confirmationModal/ConfirmationModal.tsx
+++ src/components/commonInGeneral/modal/confirmationModal/ConfirmationModal.tsx
@@ -1,6 +1,21 @@
 import type { ReactNode } from 'react'
 import Modal from '../Modal'
 
+const ConfirmationModalTitle = ({ children }: { children: ReactNode }) => {
+  return <div className="text-lg font-semibold">{children}</div>
+}
+const ConfirmationModalContent = ({ children }: { children: ReactNode }) => {
+  return <div className="mt-oz-lg">{children}</div>
+}
+
+const ConfirmationModalButtonSection = ({
+  children,
+}: {
+  children: ReactNode
+}) => {
+  return <div className="grid auto-cols-[50px]">{children}</div>
+}
+
 interface ConfirmationModalProps {
   isOn: boolean
   onClose: () => void
@@ -18,5 +33,9 @@ const ConfirmationModal = ({
     </Modal>
   )
 }
+
+ConfirmationModal.Title = ConfirmationModalTitle
+ConfirmationModal.Content = ConfirmationModalContent
+ConfirmationModal.ButtonSection = ConfirmationModalButtonSection
 
 export default ConfirmationModal
