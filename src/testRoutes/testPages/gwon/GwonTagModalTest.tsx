4ce1c8cb50e05699aa1fe35919d55cbbea7ec1da
Tynahan <nusilite@gmail.com>
2025-10-29 19:25:18 +0900

    fix: 테스트 페이지에 변경사항 반영 #170

--- src/testRoutes/testPages/gwon/GwonTagModalTest.tsx
+++ src/testRoutes/testPages/gwon/GwonTagModalTest.tsx
@@ -24,8 +24,6 @@ const GwonTagModalTest = () => {
       <TagSelectModal
         isOn={isOn}
         onClose={setIsOn} // 세터 함수로 넘겨줄것  닫거나, 취소시 선택된 태그는 날려야 해서 onClose 이벤트는 내부에서 정의
-        tagArray={tagArray}
-        setTagArray={setTagArray}
       />
       <Button onClick={onOpen}>모달 버튼</Button>
       {tagArray.length !== 0 && (
