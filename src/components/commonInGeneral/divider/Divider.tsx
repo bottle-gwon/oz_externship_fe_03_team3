8378ea19060ba4121cbc75a8b89c1059bc1b2501
Tynahan <nusilite@gmail.com>
2025-10-29 16:02:35 +0900

    style: 강의 정보 패딩 조절 #163

--- src/components/commonInGeneral/divider/Divider.tsx
+++ src/components/commonInGeneral/divider/Divider.tsx
@@ -1,5 +1,25 @@
-const Divider = () => {
-  return <div className="h-[1px] w-full bg-gray-200" />
+import type { Color } from '@/types'
+
+const bgMap = {
+  mono: 'bg-gray-200',
+  primary: 'bg-primary-200',
+  danger: 'bg-danger-border',
+  success: 'bg-success-border',
+  blue: 'bg-blue-100',
+}
+
+const Divider = ({
+  color = 'mono',
+  className,
+}: {
+  color?: Color
+  className?: string
+}) => {
+  return (
+    <div
+      className={`${className ? className : ''} ${bgMap[color]} h-[1px] w-full`}
+    />
+  )
 }
 
 export default Divider
--- src/pages/recruit/write/_RWStudyGroupInfo.tsx
+++ src/pages/recruit/write/_RWStudyGroupInfo.tsx
@@ -7,7 +7,7 @@ const LectureLine = ({ lecture }: { lecture: LectureInStudyGroup }) => {
   return (
     <Hstack className="justify-between">
       <p>{lecture.title}</p>
-      <p>{lecture.price}</p>
+      <p>{lecture.price.toLocaleString()}원</p>
     </Hstack>
   )
 }
@@ -19,15 +19,15 @@ const RWStudyGroupInfo = ({ studyGroup }: { studyGroup: StudyGroup }) => {
   }, 0)
 
   return (
-    <RoundBox color="primary" padding="lg">
-      <h4>{studyGroup.name}의 강의</h4>
+    <RoundBox color="primary" padding="lg" className="text-sm">
+      <h4 className="pb-oz-sm font-medium">선택된 그룹의 강의 정보</h4>
       {studyGroup.lectures.map((lecture) => (
         <LectureLine key={lecture.id} lecture={lecture} />
       ))}
-      <Divider />
-      <Hstack className="justify-between">
+      <Divider color="primary" className="my-oz-xs" />
+      <Hstack className="justify-between font-medium">
         <p>총 강의 비용</p>
-        <p>{totalCost}원</p>
+        <p>{totalCost.toLocaleString()}원</p>
       </Hstack>
     </RoundBox>
   )
