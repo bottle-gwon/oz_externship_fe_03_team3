b4943ea4f3ea4a4194d8967df9b6717695ad9632
bottle-gwon <lazyguy1@daum.net>
2025-10-29 16:54:35 +0900

    fix: 무한 스크롤 무한로딩 버그 수정 #159

--- src/components/chat/skeleton/ChatListSkeletonCard.tsx
+++ src/components/chat/skeleton/ChatListSkeletonCard.tsx
@@ -1,7 +1,7 @@
 import Skeleton from '@/components/commonInGeneral/skeleton/Skeleton'
 
 const ChatListSkeletonCard = () => {
-  return <Skeleton widthInPixel={300} heightInPixel={76} />
+  return <Skeleton widthInPixel={300} heightInPixel={76} className="shrink-0" />
 }
 
 export default ChatListSkeletonCard
