a36a41c2ce1cd1e6ad9a4240ed50a26f5d87d7d2
Tynahan <nusilite@gmail.com>
2025-10-30 16:40:57 +0900

    feat: 작성, 수정 라우트 분리 #196

--- src/pages/recruit/write/RecruitWritePage.tsx
+++ src/pages/recruit/write/RecruitWritePage.tsx
@@ -1,10 +1,18 @@
 import RecruitWriteContent from '@/components/recruit/write/RecruitWriteContent'
 import RecruitWriteSkeleton from '@/components/recruit/write/RecruitWriteSkeleton'
+import { useParams } from 'react-router'
 
 const RecruitWritePage = () => {
+  const params = useParams()
+  const recruitId = params.recruitId
+
   const isPending = false
   if (isPending) {
     return <RecruitWriteSkeleton />
+  }
+
+  if (!recruitId) {
+    return <RecruitWriteContent />
   }
 
   return <RecruitWriteContent />
