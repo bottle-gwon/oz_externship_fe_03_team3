20453a80035e96e5e48db6ce3046665e81787502
Tynahan <nusilite@gmail.com>
2025-10-30 17:12:47 +0900

    temp

---
+++ src/components/recruit/write/edit/RecruitEditPage.tsx
@@ -0,0 +1,30 @@
+import RecruitWritePage from '@/pages/recruit/write/RecruitWritePage'
+import useStudyHubStore from '@/store/store'
+import { useParams } from 'react-router'
+import RecruitWriteContent from '../RecruitWriteContent'
+import { useEffect } from 'react'
+import { mockRecruits } from '@/testRoutes/testPages/nari/_TestMokData'
+
+const RecruitEditPage = () => {
+  const recruitId = Number(useParams().recruitId)
+
+  // TODO: useMyRecruitQuery 여기서 호출해야
+
+  const recruitArray = useStudyHubStore((state) => state.recruitArray)
+  const setRecruitArray = useStudyHubStore((state) => state.setRecruitArray)
+  const editingRecruit = recruitArray.find(
+    (recruit) => recruit.id === recruitId
+  )
+
+  useEffect(() => {
+    setRecruitArray(mockRecruits)
+  }, [])
+
+  if (!editingRecruit) {
+    return <p>이건 생각 못 했는데...</p>
+  }
+
+  return <RecruitWriteContent editingRecruit={editingRecruit} />
+}
+
+export default RecruitEditPage
