a063fddd368df591ac227b9c3d820f650418db43
Tynahan <nusilite@gmail.com>
2025-10-31 05:45:12 +0900

    feat: 파일도 기본값 채우게 함 #196

--- src/components/recruit/write/_RWFileDropzone.tsx
+++ src/components/recruit/write/_RWFileDropzone.tsx
@@ -1,9 +1,23 @@
 import FileDropzone from '@/components/commonInGeneral/fileDropzone/FileDropzone'
 import Labeled from '@/components/commonInGeneral/inputFamily/labeled/Labeled'
-import type { RecruitWriteChildrenProps } from '@/types'
+import useStudyHubStore from '@/store/store'
+import type { FileRecord, RecruitWriteChildrenProps } from '@/types'
+import { useMemo } from 'react'
 import { Controller } from 'react-hook-form'
 
 const RWFileDropzone = ({ control, errors }: RecruitWriteChildrenProps) => {
+  const editingRecruit = useStudyHubStore((state) => state.editingRecruit)
+  const defaultFileRecord = useMemo(() => {
+    if (!editingRecruit) {
+      return undefined
+    }
+
+    return editingRecruit.attachments.reduce((acc: FileRecord, cur) => {
+      acc[cur.id] = { size: cur.size, name: cur.file_name } as File
+      return acc
+    }, {})
+  }, [editingRecruit])
+
   return (
     <Labeled isInDanger={Boolean(errors.attachments)}>
       <Labeled.Header>참고 파일 업로드</Labeled.Header>
@@ -11,7 +25,10 @@ const RWFileDropzone = ({ control, errors }: RecruitWriteChildrenProps) => {
         control={control}
         name="attachments"
         render={({ field: { onChange } }) => (
-          <FileDropzone onChange={onChange} />
+          <FileDropzone
+            onChange={onChange}
+            defaultFileRecord={defaultFileRecord}
+          />
         )}
       />
       <Labeled.Footer>{errors?.attachments?.message}</Labeled.Footer>
