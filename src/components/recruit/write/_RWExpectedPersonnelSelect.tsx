e00c9aa488deaf095080f007759a573e0fb372a0
Tynahan <nusilite@gmail.com>
2025-10-30 22:10:56 +0900

    feat: 셀렉트 메뉴에도 기본값 주입 #196

--- src/components/recruit/write/_RWExpectedPersonnelSelect.tsx
+++ src/components/recruit/write/_RWExpectedPersonnelSelect.tsx
@@ -1,5 +1,6 @@
 import Labeled from '@/components/commonInGeneral/inputFamily/labeled/Labeled'
 import Select from '@/components/commonInGeneral/select/Select'
+import useStudyHubStore from '@/store/store'
 import type { RecruitWriteChildrenProps } from '@/types'
 import { RECRUIT_WRITE_CONFIG } from '@/utils/constants'
 import { Controller } from 'react-hook-form'
@@ -8,6 +9,8 @@ const RWExpectedPersonnelSelect = ({
   errors,
   control,
 }: RecruitWriteChildrenProps) => {
+  const editingRecruit = useStudyHubStore((state) => state.editingRecruit)
+
   return (
     <Labeled isRequired isInDanger={Boolean(errors.expected_personnel)}>
       <Labeled.Header>예상 모집 인원</Labeled.Header>
@@ -15,7 +18,10 @@ const RWExpectedPersonnelSelect = ({
         control={control}
         name="expected_personnel"
         render={({ field: { onChange } }) => (
-          <Select onOptionSelect={onChange}>
+          <Select
+            onOptionSelect={onChange}
+            defaultChildren={`${editingRecruit?.expected_personnel}명`}
+          >
             <Select.Trigger>예상 모집 인원을 선택하세요</Select.Trigger>
             <Select.Content>
               {Array(RECRUIT_WRITE_CONFIG.MAX_PERSONNEL)
