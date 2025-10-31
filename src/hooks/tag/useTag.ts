b0bafe08f02edc67205c2eab13ba412b81ccf789
bottle-gwon <lazyguy1@daum.net>
2025-10-31 16:41:07 +0900

    feat: 신규 태그 추가 로직 작성 #194

--- src/hooks/tag/useTag.ts
+++ src/hooks/tag/useTag.ts
@@ -1,6 +1,6 @@
 import api from '@/api/api'
 import type { TagApiSearchParam } from '@/types'
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 
 const queryEndpoint = '/recruitments/tags'
 
@@ -13,16 +13,43 @@ const getSearchTag = async (params: TagApiSearchParam) => {
   newParam.append('page', String(page))
   newParam.append('page_size', String(page_size))
 
-  const response = await api.get(`/recruitments/tags?${newParam}`)
+  const response = await api.get(`${queryEndpoint}?${newParam}`)
+
+  return response.data
+}
+
+// 새로운 태그 추가
+// 아직 확정적이진 않아서 이부분 향후 수정할 예정입니다.
+const postNewTag = async (newTag: string) => {
+  const newTagParam = { name: newTag }
+  const response = await api.post(queryEndpoint, newTagParam)
 
   return response.data
 }
 
 //-------------------------tanstackQuery---------------------------------
 
+// 태그 검색
 export const useSearchTag = (params: TagApiSearchParam) => {
   return useQuery({
     queryKey: [queryEndpoint, params],
     queryFn: () => getSearchTag(params),
+  })
+}
+
+// 태그추가
+// 이 부분 수정중인것 같아서 향후 추가
+export const useAddNewTag = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation({
+    mutationFn: postNewTag,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [queryEndpoint] })
+    },
+    onError: () => {
+      //에러 처리
+      // console.error(e)
+    },
   })
 }
