39ebaee670d319e166b1cdcbfbafdec4443fe275
Tynahan <nusilite@gmail.com>
2025-10-31 13:58:37 +0900

    feat: 로그인 정보 로컬 스토리지에 저장 #208

--- src/store/store.ts
+++ src/store/store.ts
@@ -70,7 +70,7 @@ const useStudyHubStore = create<StudyHubState>()(
     {
       name: 'food-storage', // name of the item in the storage (must be unique)
       storage: createJSONStorage(() => localStorage), // (optional) by default, 'localStorage' is used
-      partialize: (state) => ({ fruitArray: state.fruitArray }),
+      partialize: (state) => ({ accessToken: state.accessToken, me: state.me }),
     }
   )
 )
