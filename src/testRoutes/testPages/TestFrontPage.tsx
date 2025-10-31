370852e543f3b23c0251df66a89c7ab60bd291ab
Tynahan <nusilite@gmail.com>
2025-10-31 11:30:16 +0900

    feat: 각자의 테스트 페이지와 연결 #203

--- src/testRoutes/testPages/TestFrontPage.tsx
+++ src/testRoutes/testPages/TestFrontPage.tsx
@@ -19,18 +19,78 @@ const TestFrontPage = () => {
 
           <TitledRoundBox>
             <TitledRoundBox.Title>은정님</TitledRoundBox.Title>
+            <Button onClick={() => navigate('/test/nari')}>
+              Nari Test Page
+            </Button>
+            <Button onClick={() => navigate('/test/narimodal')}>
+              Application Modal Page
+            </Button>
+            <Button onClick={() => navigate('/test/narimanage')}>
+              Recruit Management Page
+            </Button>
+            <Button onClick={() => navigate('/test/narimanage1')}>
+              How To Use Select
+            </Button>
           </TitledRoundBox>
 
           <TitledRoundBox>
             <TitledRoundBox.Title>혜정님</TitledRoundBox.Title>
+            <Button onClick={() => navigate('/test/hyejeong')}>
+              Recommend Section
+            </Button>
+            <Button onClick={() => navigate('/test/hyejeong/title')}>
+              Recruit PAge
+            </Button>
+            <Button onClick={() => navigate('/test/hyejeong/recruitlist')}>
+              Recruit Page
+            </Button>
+            <Button onClick={() => navigate('/test/hyejeong/subheader')}>
+              Sub Header
+            </Button>
+            <Button onClick={() => navigate('/test/hyejeong/apllicantcard')}>
+              Applicant Card Modal
+            </Button>
+            <Button onClick={() => navigate('/test/hyejeong/managemodal')}>
+              Manage Modal
+            </Button>
+            <Button onClick={() => navigate('/test/hyejeong/managedetail')}>
+              Manage Detail Modal
+            </Button>
           </TitledRoundBox>
 
           <TitledRoundBox>
             <TitledRoundBox.Title>병권님</TitledRoundBox.Title>
+            <Button onClick={() => navigate('/test/gwon')}>Test Page</Button>
+            <Button onClick={() => navigate('/test/gwon/modal')}>
+              Modal Test
+            </Button>
+            <Button onClick={() => navigate('/test/gwon/floating')}>
+              Floating
+            </Button>
+            <Button onClick={() => navigate('/test/gwon/chat')}>Chat</Button>
           </TitledRoundBox>
 
           <TitledRoundBox>
             <TitledRoundBox.Title>하흥주</TitledRoundBox.Title>
+            <Button onClick={() => navigate('/test/thepott')}>Test Page</Button>
+            <Button onClick={() => navigate('/test/thepott/button')}>
+              Button Page
+            </Button>
+            <Button onClick={() => navigate('/test/thepott/roundbox')}>
+              Roundbox Page
+            </Button>
+            <Button onClick={() => navigate('/test/thepott/modal')}>
+              Modal Page
+            </Button>
+            <Button onClick={() => navigate('/test/thepott/input-family')}>
+              Markdown Page
+            </Button>
+            <Button onClick={() => navigate('/test/thepott/markdown')}>
+              Skeleton Page
+            </Button>
+            <Button onClick={() => navigate('/test/thepott/file-dropzone')}>
+              File Dropzone
+            </Button>
           </TitledRoundBox>
         </Vstack>
       </RoundBox>
