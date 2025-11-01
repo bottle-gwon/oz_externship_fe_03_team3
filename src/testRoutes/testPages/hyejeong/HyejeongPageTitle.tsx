fd625ca85eca4da323ab0328e6e5eef86b79dd16
Hyejeong00 <love72771112@gmail.com>
2025-11-01 23:50:10 +0900

    feat: TitleSection 테스트파일 삭제

--- src/testRoutes/testPages/hyejeong/HyejeongPageTitle.tsx
+++
@@ -1,8 +0,0 @@
-import TitleSection from '@/components/titleSection/TitleSection'
-
-const HyejeongPageTitle = () => {
-  const isLoggedIn = false
-  return <TitleSection type="manage" isLoggedIn={isLoggedIn} />
-}
-
-export default HyejeongPageTitle
