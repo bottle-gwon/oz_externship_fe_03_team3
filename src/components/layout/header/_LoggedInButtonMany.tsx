9eeddc78309e5002a52cbe933a778d87df030c95
Tynahan <nusilite@gmail.com>
2025-11-03 16:20:54 +0900

    temp

--- src/components/layout/header/_LoggedInButtonMany.tsx
+++ src/components/layout/header/_LoggedInButtonMany.tsx
@@ -4,15 +4,38 @@ import useStudyHubStore from '@/store/store'
 import { Bell } from 'lucide-react'
 import type { Me } from '@/types'
 import ProfileImage from '@/components/commonInProject/ProfileImage/ProfileImage'
+import Dropdown from '@/components/commonInGeneral/dropdown/Dropdown'
 
 const ProfileButton = ({ me }: { me: Me }) => {
+  const handleChange = (value: string) => {
+    switch (value) {
+      case 'mypage':
+        console.log('---- mypage')
+        return
+      case 'logout':
+        console.log('---- logout')
+    }
+  }
   return (
-    <Button color="primary" variant="ghost" size="lg" className="py-oz-sm p-0">
-      <Hstack className="items-center">
-        <ProfileImage url={me.profile_image_url} />
-        <p className="shrink-0 text-gray-700">{me.name}</p>
-      </Hstack>
-    </Button>
+    <Dropdown>
+      <Dropdown.Trigger>
+        <Button
+          color="primary"
+          variant="ghost"
+          size="lg"
+          className="py-oz-sm p-0"
+        >
+          <Hstack className="items-center">
+            <ProfileImage url={me.profile_image_url} />
+            <p className="shrink-0 text-gray-700">{me.name}</p>
+          </Hstack>
+        </Button>
+      </Dropdown.Trigger>
+      <Dropdown.Menu onChange={handleChange}>
+        <Dropdown.MenuItem value="mypage">마이페이지</Dropdown.MenuItem>
+        <Dropdown.MenuItem value="logout">로그아웃</Dropdown.MenuItem>
+      </Dropdown.Menu>
+    </Dropdown>
   )
 }
 
