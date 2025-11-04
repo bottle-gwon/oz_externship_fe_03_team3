966f266e9bd47c5d5020f4d8054319fe2e98c567
Tynahan <nusilite@gmail.com>
2025-11-04 14:04:22 +0900

    chore: useQuery 사용 팁 주석 작성 #243

--- src/components/lecture/lectureCard/expandedReviewSection/ExpandedReviewSection.tsx
+++ src/components/lecture/lectureCard/expandedReviewSection/ExpandedReviewSection.tsx
@@ -4,11 +4,10 @@ import ReviewCard from './_ReviewCard'
 import { useQuery } from '@tanstack/react-query'
 import api from '@/api/api'
 
-/*
- * lecture - 통으로 받아와서 마운트 되면 요청을 보냅니다
- */
 const ExpandedReviewSection = ({ lecture }: { lecture: Lecture }) => {
   // NOTE: 현재 빈배열 응답이 옵니다. 익스프레스 서버를 이용해서 테스트해야 합니다
+  // NOTE: data를 이 컴포넌트에서만 참조하기 때문에 여기서 useQuery를 호출합니다
+  // NOTE: 컴포넌트 구조가 복잡하다면 커스텀 후크와 페이지 전용 zustand store를 구축하는 편이 좋습니다
   const endpoint = `/lectures/${lecture.uuid}/reviews`
   const { data, isPending, error } = useQuery({
     queryKey: [endpoint],
