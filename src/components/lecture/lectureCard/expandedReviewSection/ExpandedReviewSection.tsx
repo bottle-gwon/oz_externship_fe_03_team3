28d440a769e0f439147fa292ceb2e13444b7d9d1
Tynahan <nusilite@gmail.com>
2025-11-04 14:02:34 +0900

    feat: 더미 데이터를 api에서 받은 것을 대체 #243

--- src/components/lecture/lectureCard/expandedReviewSection/ExpandedReviewSection.tsx
+++ src/components/lecture/lectureCard/expandedReviewSection/ExpandedReviewSection.tsx
@@ -1,19 +1,36 @@
-import type { Lecture } from '@/types'
-import { getDummyReviewArray } from '../../dummyApi'
+import type { Lecture, LectureReview } from '@/types'
 import { Vstack } from '@/components/commonInGeneral/layout'
 import ReviewCard from './_ReviewCard'
+import { useQuery } from '@tanstack/react-query'
+import api from '@/api/api'
 
 /*
  * lecture - 통으로 받아와서 마운트 되면 요청을 보냅니다
  */
 const ExpandedReviewSection = ({ lecture }: { lecture: Lecture }) => {
-  const reviewArray = getDummyReviewArray(lecture)
-  reviewArray.splice(4) // 백엔드에서 넷 이상을 보내줄 경우 대비
+  // NOTE: 현재 빈배열 응답이 옵니다. 익스프레스 서버를 이용해서 테스트해야 합니다
+  const endpoint = `/lectures/${lecture.uuid}/reviews`
+  const { data, isPending, error } = useQuery({
+    queryKey: [endpoint],
+    queryFn: async () => (await api.get(endpoint)).data.reviews,
+  })
+
+  if (isPending) {
+    return <p>여기에 넣을 스켈레톤을 만들어야 합니다</p>
+  }
+
+  if (error) {
+    return <p>여기에 넣을 오류 메시지 컴포넌트를 만들어야 합니다</p>
+  }
+
+  if (data.length === 0) {
+    return <p>리뷰가 없습니다 컴포넌트를 만들어야 합니다</p>
+  }
 
   return (
     <Vstack className="mt-oz-lg pt-oz-lg gap-oz-md border-t border-t-gray-200">
       <h3 className="font-medium">최근 리뷰</h3>
-      {reviewArray.map((review) => (
+      {data?.map((review: LectureReview) => (
         <ReviewCard key={review.id} review={review} />
       ))}
     </Vstack>
