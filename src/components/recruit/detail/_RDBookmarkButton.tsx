e699c6109ff17eea9033183d6f1b4973a97e94ce
Tynahan <nusilite@gmail.com>
2025-11-06 08:56:05 +0900

    style: 북마크 되었을 때의 스타일 수정 #265

--- src/components/recruit/detail/_RDBookmarkButton.tsx
+++ src/components/recruit/detail/_RDBookmarkButton.tsx
@@ -16,7 +16,10 @@ const RDBookmarkButton = ({
       shape={isWide ? 'rectangle' : 'square'}
       color={recruitDetail.is_bookmarked ? 'primary' : 'mono'}
     >
-      <Bookmark size={isWide ? 16 : undefined} />
+      <Bookmark
+        size={isWide ? 16 : undefined}
+        fill={recruitDetail.is_bookmarked ? 'white' : undefined}
+      />
       {isWide && '북마크'}
     </Button>
   )
