e319fc2829dd5314ce042adc8642fdeaee157cd9
Tynahan <nusilite@gmail.com>
2025-11-06 17:23:06 +0900

    feat: 토글 디바운스 이식 #279

--- src/components/recruit/detail/_RDBookmarkButton.tsx
+++ src/components/recruit/detail/_RDBookmarkButton.tsx
@@ -1,7 +1,9 @@
 import Button from '@/components/commonInGeneral/button/Button'
 import useRecruitDetailMutation from '@/hooks/recruitDetail/useRecruitDetailMutation'
+import useDebounceToggle from '@/hooks/useDebounceToggle'
 import type { RecruitDetail } from '@/types'
 import { Bookmark } from 'lucide-react'
+import { useEffect } from 'react'
 
 const RDBookmarkButton = ({
   recruitDetail,
@@ -11,22 +13,31 @@ const RDBookmarkButton = ({
   isWide?: boolean
 }) => {
   const { toggleBookmarkMutation } = useRecruitDetailMutation(recruitDetail)
+  const { debouncedBoolValue, realTimeBoolValue, toggleBoolValue } =
+    useDebounceToggle(recruitDetail.is_bookmarked)
 
-  const handleClick = () => {
+  useEffect(() => {
+    if (debouncedBoolValue === recruitDetail.is_bookmarked) {
+      return
+    }
+
     const newOne: RecruitDetail = {
       ...recruitDetail,
       is_bookmarked: !recruitDetail.is_bookmarked,
     }
     toggleBookmarkMutation.mutate({ data: undefined, newOne })
-  }
+
+    // NOTE: recruitDetail 넣으면 무한 렌더링 일어남
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedBoolValue])
 
   return (
     <Button
-      variant={recruitDetail.is_bookmarked ? 'contained' : 'outlined'}
+      variant={realTimeBoolValue ? 'contained' : 'outlined'}
       size="lg"
       shape={isWide ? 'rectangle' : 'square'}
-      color={recruitDetail.is_bookmarked ? 'primary' : 'mono'}
-      onClick={handleClick}
+      color={realTimeBoolValue ? 'primary' : 'mono'}
+      onClick={toggleBoolValue}
     >
       <Bookmark size={isWide ? 16 : undefined} />
       {isWide && '북마크'}
