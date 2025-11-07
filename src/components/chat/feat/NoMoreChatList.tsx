5adf15a9f995ec54c1e73d3f0639066b1d3ea451
bottle-gwon <lazyguy1@daum.net>
2025-11-07 15:28:23 +0900

    feat: 채팅방 더 불러올 데이터 없으면 안내 메시지 출력 #278

---
+++ src/components/chat/feat/NoMoreChatList.tsx
@@ -0,0 +1,11 @@
+import { Hstack } from '@/components/commonInGeneral/layout'
+
+const NoMoreChatList = () => {
+  return (
+    <Hstack className="items-center justify-center">
+      <p>불러올 채팅방이 없습니다.</p>
+    </Hstack>
+  )
+}
+
+export default NoMoreChatList
