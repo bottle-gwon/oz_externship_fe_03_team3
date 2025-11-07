eb50342c1cfc2a88bdd8908138ef65e524e71069
Tynahan <nusilite@gmail.com>
2025-11-07 19:55:26 +0900

    feat: suspense fallback, pending 스켈레톤 적용 #301

--- src/components/recruit/detail/RecruitDetailSkeleton.tsx
+++ src/components/recruit/detail/RecruitDetailSkeleton.tsx
@@ -1,5 +1,20 @@
+import { Container, Vstack } from '@/components/commonInGeneral/layout'
+import Skeleton from '@/components/commonInGeneral/skeleton/Skeleton'
+
 const RecruitDetailSkeleton = () => {
-  return <div></div>
+  return (
+    <Container width="md" className="py-oz-xxl">
+      <Vstack gap="xxl">
+        <Skeleton widthInPixel={500} heightInPixel={500} />
+
+        <Skeleton className="w-full" heightInPixel={500} />
+
+        <Skeleton className="w-full" heightInPixel={500} />
+        <Skeleton className="w-full" heightInPixel={500} />
+        <Skeleton className="w-full" heightInPixel={500} />
+      </Vstack>
+    </Container>
+  )
 }
 
 export default RecruitDetailSkeleton
