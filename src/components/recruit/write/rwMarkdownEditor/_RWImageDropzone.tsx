63a1eaa57dbc3a26dc6e64e7e751c2bdd582ff97
Tynahan <nusilite@gmail.com>
2025-11-07 15:04:11 +0900

    chore: 테스트용 주석 작성 #293

--- src/components/recruit/write/rwMarkdownEditor/_RWImageDropzone.tsx
+++ src/components/recruit/write/rwMarkdownEditor/_RWImageDropzone.tsx
@@ -4,11 +4,18 @@ import { useState } from 'react'
 const RWImageDropzone = () => {
   const [isDragEntered, setIsDragEntered] = useState(false)
 
+  const handleFileArrayChange = (_fileArray: File[]) => {
+    // NOTE: 테스트할 땐 아래 주석을 해제해주세요
+    // console.log({_fileArray})
+    //   debugger
+    // ---- 여기까지
+  }
+
   return (
     <FileDropLayer
-      onFileArrayChange={(fileArray) => {}}
+      onFileArrayChange={handleFileArrayChange}
       onDragEnterChange={setIsDragEntered}
-      className={`absolute top-0 left-0 h-full w-full ${isDragEntered ? 'bg-amber-100' : 'bg-blue-300'}`}
+      className={`absolute top-0 left-0 h-full w-full ${isDragEntered ? 'bg-amber-100' : ''}`}
     />
   )
 }
