16c92353cccd8e8b9bcbb7dbe3140047531251b4
Tynahan <nusilite@gmail.com>
2025-11-07 20:02:43 +0900

    style: 스켈레톤 높이 조절 #301

--- src/components/recruit/detail/RecruitDetailSkeleton.tsx
+++ src/components/recruit/detail/RecruitDetailSkeleton.tsx
@@ -3,15 +3,17 @@ import Skeleton from '@/components/commonInGeneral/skeleton/Skeleton'
 
 const RecruitDetailSkeleton = () => {
   return (
-    <Container width="md" className="py-oz-xxl">
+    <Container width="md" className="py-oz-xxl h-full overflow-hidden">
       <Vstack gap="xxl">
-        <Skeleton widthInPixel={500} heightInPixel={500} />
+        <Skeleton widthInPixel={40} heightInPixel={40} radius="full" />
 
-        <Skeleton className="w-full" heightInPixel={500} />
+        <Skeleton className="w-full" heightInPixel={337} radius="lg" />
 
-        <Skeleton className="w-full" heightInPixel={500} />
-        <Skeleton className="w-full" heightInPixel={500} />
-        <Skeleton className="w-full" heightInPixel={500} />
+        <Skeleton className="w-full" heightInPixel={1000} radius="lg" />
+
+        <Skeleton className="w-full" heightInPixel={490} radius="lg" />
+        <Skeleton className="w-full" heightInPixel={308} radius="lg" />
+        <Skeleton className="w-full" heightInPixel={97} radius="lg" />
       </Vstack>
     </Container>
   )
