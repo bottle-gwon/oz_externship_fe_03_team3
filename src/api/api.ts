baf0ddad3d080f82461d343db38750d1494df869
Tynahan <nusilite@gmail.com>
2025-11-09 20:04:20 +0900

    feat: axios interceptor로 액세스 토큰 받는 로직 수정 #305

--- src/api/api.ts
+++ src/api/api.ts
@@ -10,55 +10,45 @@ const api = axios.create({
   timeout: 5000,
 })
 
+export const subApi = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+  headers: { Accept: 'application/json' },
+  timeout: 5000,
+})
+
 // 요청 인터셉터
-api.interceptors.request.use((config) => {
+// NOTE: 액세스 토큰은 요청 실패하고 발급받는 게 아니라 요청 전에 없으면 요청합니다
+api.interceptors.request.use(async (config) => {
   const state = useStudyHubStore.getState()
-  const accessToken = state.accessToken
+  let accessToken = state.accessToken
 
   if (accessToken) {
     config.headers.Authorization = `Bearer ${accessToken}`
+    return config
   }
 
-  return config
+  try {
+    const response = await subApi.post('/auth/refresh')
+    accessToken = response.data.data.access
+
+    const responseMe = await subApi.get('/users/me ', {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    })
+    const me = responseMe.data
+
+    const state = useStudyHubStore.getState()
+    const setAccessToken = state.setAccessToken
+    const setMe = state.setMe
+
+    setAccessToken(accessToken)
+    setMe(me)
+
+    config.headers.Authorization = `Bearer ${accessToken}`
+    return config
+  } catch {
+    return config
+  }
 })
-
-// 응답 인터셉터
-api.interceptors.response.use(
-  (res) => res,
-  async (error) => {
-    if (error.response?.status === 401) {
-      // 토큰 재발급 시도
-      try {
-        const response = await api.post('/auth/refresh')
-
-        const statusCode = response.status
-        const responseData = response.data
-
-        if (statusCode !== 200) {
-          throw new Error(`${statusCode}에러 ${responseData?.data.error}`)
-        }
-
-        // 발급 받은 access토큰 저장
-        const state = useStudyHubStore.getState()
-        const setAccessToken = state.setAccessToken
-        const newAccessToken = responseData.data.access_token
-        setAccessToken(newAccessToken)
-
-        // 요청 재시도
-        return api(error.config)
-      } catch (error) {
-        // 재발급 실패 -> 로그인 페이지로
-        window.location.href = import.meta.env.VITE_LOGIN_PAGE_URL
-
-        if (error instanceof Error) {
-          throw error
-        } else {
-          throw new Error(String(error))
-        }
-      }
-    }
-    return Promise.reject(error)
-  }
-)
 
 export default api
