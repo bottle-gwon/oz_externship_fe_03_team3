c41221e07c46720803704fec57158aefe4f0f438
Hyejeong00 <love72771112@gmail.com>
2025-11-10 05:14:45 +0900

    feat: eslint warning 해결

--- src/hooks/useOneWayInfinityScroll.ts
+++ src/hooks/useOneWayInfinityScroll.ts
@@ -19,11 +19,12 @@ const useOneWayInfinityScroll = (
   }, [callback])
 
   useEffect(() => {
-    const target = targetRef.current
-    if (!target) return
+    // 타겟 요소 없으면 중지
+    if (!targetRef.current) return
 
     const { root = null, threshold = 1.0, rootMargin = '0px' } = options || {}
 
+    //옵저버 없으면 생성
     if (!observerRef.current) {
       observerRef.current = new IntersectionObserver(
         (entries) => {
@@ -35,12 +36,17 @@ const useOneWayInfinityScroll = (
       )
     }
 
-    observerRef.current.observe(target)
+    //타겟 관찰
+    observerRef.current.observe(targetRef.current)
 
     return () => {
-      observerRef.current?.disconnect()
-      observerRef.current = null
+      // 옵저버 해제 및 초기화
+      if (observerRef.current) {
+        observerRef.current.disconnect()
+        observerRef.current = null
+      }
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [targetRef.current, options])
 }
 
