cf98fac216aedc34d59e12cfc1f8beaf9842adcb
Tynahan <nusilite@gmail.com>
2025-11-10 08:15:20 +0900

    fix: 만료 로직 복구 #305

--- src/api/api.ts
+++ src/api/api.ts
@@ -10,6 +10,7 @@ const api = axios.create({
   timeout: 5000,
 })
 
+// NOTE: 인터셉터 안에서 api를 쓰면 무한 요청이 일어납니다
 export const subApi = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
@@ -17,8 +18,27 @@ export const subApi = axios.create({
   timeout: 5000,
 })
 
+const getRefreshAndMe = async (): Promise<string> => {
+  const response = await subApi.post('/auth/refresh')
+  const accessToken = response.data.data.access
+
+  const responseMe = await subApi.get('/users/me ', {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  })
+  const me = responseMe.data
+
+  const state = useStudyHubStore.getState()
+  const setAccessToken = state.setAccessToken
+  const setMe = state.setMe
+
+  setAccessToken(accessToken)
+  setMe(me)
+
+  return accessToken
+}
+
 // 요청 인터셉터
-// NOTE: 액세스 토큰은 요청 실패하고 발급받는 게 아니라 요청 전에 없으면 요청합니다
+// NOTE: 액세스 토큰이 없다면 요청 전에 받습니다
 api.interceptors.request.use(async (config) => {
   const state = useStudyHubStore.getState()
   let accessToken = state.accessToken
@@ -29,26 +49,32 @@ api.interceptors.request.use(async (config) => {
   }
 
   try {
-    const response = await subApi.post('/auth/refresh')
-    accessToken = response.data.data.access
-
-    const responseMe = await subApi.get('/users/me ', {
-      headers: { Authorization: `Bearer ${accessToken}` },
-    })
-    const me = responseMe.data
-
-    const state = useStudyHubStore.getState()
-    const setAccessToken = state.setAccessToken
-    const setMe = state.setMe
-
-    setAccessToken(accessToken)
-    setMe(me)
-
+    accessToken = await getRefreshAndMe()
     config.headers.Authorization = `Bearer ${accessToken}`
     return config
   } catch {
     return config
   }
 })
+
+// NOTE: 가지고 있는 토큰이 만료되었다고 오류를 받으면 재발급 받습니다
+api.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    if (error.response?.status !== 401) {
+      return Promise.reject(error)
+    }
+
+    // 1) 토큰 재발급 시도 엔드포인트 호출 (팀 규칙에 맞게)
+    try {
+      await getRefreshAndMe()
+      // 2) 실패했던 요청 재시도
+      return api(error.config)
+    } catch {
+      // 3) 재발급 실패 → 로그인 페이지로
+      window.location.href = '/login'
+    }
+  }
+)
 
 export default api
