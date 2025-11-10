9f3407863a153cb9a0e7715c12c65202e7e2a426
Hyejeong00 <love72771112@gmail.com>
2025-11-10 13:09:36 +0900

    feat: Pending 상태일 떄 스켈레톤 제작

---
+++ src/components/recruit/manageModal/skeleton/ApplicantTitleSkeleton.tsx
@@ -0,0 +1,7 @@
+import Skeleton from '@/components/commonInGeneral/skeleton/Skeleton'
+
+const ApplicantTitleSkeleton = () => {
+  return <Skeleton heightInPixel={20} widthInPixel={357} radius="sm" />
+}
+
+export default ApplicantTitleSkeleton
