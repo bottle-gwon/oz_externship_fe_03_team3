ce8054fbc7aaf3a0c2262286706afe34bd0d78f4
Tynahan <nusilite@gmail.com>
2025-11-10 12:45:19 +0900

    feat: 바닥에서 300 px 떨어져있을 때 다음 페이지 요청 #312

--- src/hooks/useOneWayInfinityScroll.ts
+++ src/hooks/useOneWayInfinityScroll.ts
@@ -21,7 +21,6 @@ const useOneWayInfinityScroll = (
   useEffect(() => {
     // 타겟 요소 없으면 중지
     if (!targetRef.current) return
-
     const { root = null, threshold = 1.0, rootMargin = '0px' } = options || {}
 
     //옵저버 없으면 생성
