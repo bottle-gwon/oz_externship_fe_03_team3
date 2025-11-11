0f229e741d8f362e52632f23ec316882614b40cc
bottle-gwon <lazyguy1@daum.net>
2025-11-11 11:29:34 +0900

    feat: 채팅방 초기 스크롤(임시) 설정 #297

--- src/components/chat/feat/ChatDisplay.tsx
+++ src/components/chat/feat/ChatDisplay.tsx
@@ -2,7 +2,7 @@ import { Vstack } from '@/components/commonInGeneral/layout'
 import ChatBox from './ChatBox'
 import ChattingRoomSkeleton from '../skeleton/ChattingRoomSkeleton'
 import Skeleton from '@/components/commonInGeneral/skeleton/Skeleton'
-import { useEffect, useRef } from 'react'
+import { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import useStudyHubStore from '@/store/store'
 import { useVirtualizer } from '@tanstack/react-virtual'
 
@@ -21,27 +21,33 @@ const ChatDisplay = ({
   const containerRef = useRef<HTMLDivElement>(null)
   const overflow = isPending ? 'overflow-hidden' : 'overflow-y-scroll'
   const chatMessageArray = useStudyHubStore((state) => state.chatMessageArray)
+  const [init, setInit] = useState(true) // 초기 스크롤 세팅할 플래그
 
   // 윈도잉(가상화 리스트)
   const rowVirtualizer = useVirtualizer({
     count: chatMessageArray.length, // 렌더링할 아이템 개수
     getScrollElement: () => containerRef.current, //스크롤 요소
-    estimateSize: (_) => 80, // 각 메시지 예상 높이 여기서는 최저 높이로 설정 했습니다.
-    overscan: 3, // 화면 바깥에 미리 렌더링 할 메시지 수
+    estimateSize: (_) => 120, // 각 메시지 예상 높이
+    overscan: 20, // 화면 바깥에 미리 렌더링 할 메시지 수
   })
 
-  useEffect(() => {
+  // 처음 들어오면 아래로 이동하는 useEffect
+  // Todo 추후에 안읽은 메시지로 이동 하도록 변경
+  useLayoutEffect(() => {
     if (!containerRef) {
       return
     }
-    if (containerRef.current) {
-      // const scrollTop = containerRef.current.scrollHeight - scrollHeight;
-
-      // 추후에 안읽은 메시지로 이동 하도록 변경
-      containerRef.current.scrollTop = 20
-      // setScrollHeight(containerRef.current.scrollHeight)
+    if (containerRef.current && init && !isPending) {
+      rowVirtualizer.scrollToIndex(chatMessageArray.length - 1, {
+        align: 'end',
+      })
+      setInit(false)
+    } else if (containerRef.current) {
+      rowVirtualizer.scrollToIndex(2, { align: 'end' })
     }
-  }, [containerRef])
+  }, [rowVirtualizer, chatMessageArray.length, init, isPending])
+
+  useEffect(() => {}, [])
 
   return (
     <Vstack
@@ -52,7 +58,7 @@ const ChatDisplay = ({
       <div className={`relative w-full h-[${rowVirtualizer.getTotalSize()}]`}>
         {/* 가상화 리스트 적용 */}
         <Vstack
-          className="absolute w-full"
+          className="absolute top-0 left-0 w-full"
           style={{
             transform: `translateY(${rowVirtualizer.getVirtualItems()[0]?.start ?? 0}px)`,
           }}
