0f98714ecc29b9e3c1bc63f21a979e0e52891319
nari-0v0 <ej9811@naver.com>
2025-11-11 19:07:03 +0900

    feat : 공고관리 스켈레톤 제작 #333

---
+++ src/components/recruit/manage/ManageSkeleton.tsx
@@ -0,0 +1,35 @@
+import { Container, Hstack, Vstack } from '@/components/commonInGeneral/layout'
+import Skeleton from '@/components/commonInGeneral/skeleton/Skeleton'
+
+const ManageSkeleton = () => {
+  return (
+    <Container className="py-oz-xxl h-full overflow-hidden" isPadded>
+      <Vstack gap="xxl">
+        <Hstack gap="none" className="items-center justify-between">
+          <Hstack className="gap-oz-md items-center">
+            <Skeleton heightInPixel={40} widthInPixel={40} radius="lg" />
+            <Vstack gap="sm">
+              <Skeleton heightInPixel={40} widthInPixel={200} />
+              <Skeleton heightInPixel={20} widthInPixel={300} />
+            </Vstack>
+          </Hstack>
+          <Skeleton heightInPixel={50} widthInPixel={160} radius="lg" />
+        </Hstack>
+
+        <Hstack>
+          <Skeleton heightInPixel={80} widthInPixel={248} radius="lg" />
+          <Skeleton heightInPixel={80} widthInPixel={248} radius="lg" />
+          <Skeleton heightInPixel={80} widthInPixel={248} radius="lg" />
+        </Hstack>
+
+        <Skeleton heightInPixel={120} radius="lg" />
+        <Vstack>
+          <Skeleton heightInPixel={24} widthInPixel={160} radius="lg" />
+          <Skeleton heightInPixel={1192} radius="lg" />
+        </Vstack>
+      </Vstack>
+    </Container>
+  )
+}
+
+export default ManageSkeleton
