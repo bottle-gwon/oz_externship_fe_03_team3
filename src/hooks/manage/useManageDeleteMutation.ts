b7ba1199e9782df38066f825e001582534a7a1a4
nari-0v0 <ej9811@naver.com>
2025-11-11 12:57:55 +0900

    refactor : 공고관리 카드 삭제 훅으로 변경 #319

---
+++ src/hooks/manage/useManageDeleteMutation.ts
@@ -0,0 +1,55 @@
+import api from '@/api/api'
+import type { RecruitsManageResponse, RecruitDelete } from '@/types'
+import type { InfiniteData } from '@tanstack/react-query'
+import useSimpleMutation from '../useSimpleMutation'
+
+const updateManageDeleteCache = (
+  previous: InfiniteData<RecruitsManageResponse, unknown>,
+  removedRecruit: RecruitDelete
+) => ({
+  ...previous,
+  pages: previous.pages.map((page) => {
+    const filteredResults = page.results.filter(
+      (recruitment) => recruitment.id !== removedRecruit.id
+    )
+
+    const pageStatus = page.status
+    const { total, open, closed } = page.count
+    const decreaseOpen = removedRecruit.is_closed ? 0 : 1
+    const decreaseClosed = removedRecruit.is_closed ? 1 : 0
+
+    const updatedCount = {
+      total: Math.max(0, total - 1),
+      open: pageStatus === 'closed' ? open : Math.max(0, open - decreaseOpen),
+      closed:
+        pageStatus === 'open' ? closed : Math.max(0, closed - decreaseClosed),
+    }
+
+    return {
+      ...page,
+      results: filteredResults,
+      count: updatedCount,
+    }
+  }),
+})
+
+const useManageDeleteMutation = (
+  userId: number,
+  opts?: { onSuccess?: () => void; onError?: (error: Error) => void }
+) => {
+  const queryEndpoint = `/recruitments/user/${userId}`
+
+  const deleteRecruitmentMutation = useSimpleMutation({
+    queryEndpoint,
+    mutationFnWithData: async (recruitmentId: number) => {
+      await api.delete(`/recruitments/${recruitmentId}`)
+    },
+    updateCacheForUi: updateManageDeleteCache,
+    handleSuccess: opts?.onSuccess,
+    handleError: opts?.onError,
+  })
+
+  return { deleteRecruitmentMutation }
+}
+
+export default useManageDeleteMutation
