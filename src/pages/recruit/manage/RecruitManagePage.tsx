9dd6af24374c594f62da01263ab7c32184ece8f2
nari-0v0 <ej9811@naver.com>
2025-11-12 15:18:46 +0900

    chore : 진행상황 주석 추가 #334

--- src/pages/recruit/manage/RecruitManagePage.tsx
+++ src/pages/recruit/manage/RecruitManagePage.tsx
@@ -7,6 +7,7 @@ const RecruitManagePage = () => {
   const accessToken = useStudyHubStore((state) => state.accessToken)
   const userId = 24
   //실제 api 연결 시 주석해제 후 위 userId 삭제
+  //현재 명세서 상은 user_id 앤드포인트를 엑세스 토큰값에서 불러와야 되는거같아 문의드림. 답변오는대로 리펙토링 예정.
   // const userId = useStudyHubStore((state) => state.me?.id ?? 0)
   const { isPending } = useRecruitManage(userId)
   if (!accessToken) {
