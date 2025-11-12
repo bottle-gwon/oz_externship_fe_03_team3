617c46601dc2767a157da434c1ed82f18270b7aa
Tynahan <nusilite@gmail.com>
2025-11-12 13:41:42 +0900

    fix: trailing slash 삭제 #339

--- src/hooks/recruitDetail/useRecruitDetailQuery.ts
+++ src/hooks/recruitDetail/useRecruitDetailQuery.ts
@@ -3,8 +3,7 @@ import type { RecruitDetail } from '@/types'
 import { useQuery } from '@tanstack/react-query'
 
 const useRecruitDetailQuery = (recruitId: number) => {
-  // NOTE: 공고 세부 페이지 엔드포인트 마지막을 `/`로 해서 여기서도 그리 함
-  const endpoint = `/recruitments/${recruitId}/`
+  const endpoint = `/recruitments/${recruitId}`
   const { data, isPending, error } = useQuery({
     queryKey: [endpoint],
     queryFn: async () => (await api.get(endpoint)).data as RecruitDetail,
