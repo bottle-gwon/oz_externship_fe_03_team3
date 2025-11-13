d99de6717582091cf62ae5f36ca7bf7ee52dd83a
Tynahan <nusilite@gmail.com>
2025-11-13 10:59:23 +0900

    feat: 쿼리를 버튼 마운트 시점에 진행 #350

--- src/components/layout/header/notification/NotificationButton.tsx
+++ src/components/layout/header/notification/NotificationButton.tsx
@@ -4,8 +4,21 @@ import Skeleton from '@/components/commonInGeneral/skeleton/Skeleton'
 import { Bell } from 'lucide-react'
 import { Suspense } from 'react'
 import NotificationBox from './_NotificationBox'
+import useNotificationStore from '@/store/notification/notificationStore'
+import useNotificationsQuery from '@/hooks/notification/useNotificationsQuery'
+import RoundBox from '@/components/commonInGeneral/roundBox/RoundBox'
 
 const NotificationButton = () => {
+  const notificationArray = useNotificationStore(
+    (state) => state.notificationArray
+  )
+  const selectedTab = useNotificationStore((state) => state.selectedTab)
+
+  const { isPending, error } = useNotificationsQuery()
+
+  const isShowingSkeleton =
+    isPending && notificationArray.length === 0 && selectedTab === 'all'
+
   return (
     <Dropdown>
       <Dropdown.Trigger>
@@ -14,6 +27,12 @@ const NotificationButton = () => {
         </Button>
       </Dropdown.Trigger>
       <Dropdown.Content>
+        {/* NOTE: 읽음, 읽지 않음 탭에선 전체 알림을 필터링한 것을 스켈레톤 대용으로 사용하기에 스켈레톤이 필요 없습니다 */}
+        {isShowingSkeleton && (
+          <Skeleton heightInPixel={475} widthInPixel={384} />
+        )}
+        {/* TODO: 여기 에러 컴포넌트도 작게 만들어야 함 */}
+        {error && <RoundBox>알 수 없는 에러가 발생했습니다</RoundBox>}
         <Suspense
           fallback={<Skeleton heightInPixel={475} widthInPixel={384} />}
         >
