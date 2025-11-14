d0748dca831809cc859408b5a1507b9c970a6011
Tynahan <nusilite@gmail.com>
2025-11-14 20:20:36 +0900

    fix: 북마크 낙관적 업데이트 안 되던 것 수정 #376

--- src/hooks/recruitDetail/useRecruitDetailMutation.ts
+++ src/hooks/recruitDetail/useRecruitDetailMutation.ts
@@ -3,9 +3,9 @@ import useSimpleMutation from '../useSimpleMutation'
 import api from '@/api/api'
 
 const useRecruitDetailMutation = (recruitDetail: RecruitDetail) => {
-  const queryEndpoint = `/recruitments/${recruitDetail.uuid}/`
+  const queryEndpoint = `/recruitments/${recruitDetail.uuid}`
   const toggleBookmarkMutation = useSimpleMutation({
-    queryEndpoint: queryEndpoint,
+    queryEndpoint,
     mutationFnWithData: () =>
       api.post(`/recruitments/bookmarks/${recruitDetail.uuid}`),
     updateCacheForUi: (_previous: RecruitDetail, newOne: RecruitDetail) => {
