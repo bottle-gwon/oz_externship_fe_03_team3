de2ba6b256ae40c4f4b8a71e2fc2008a629875e4
Tynahan <nusilite@gmail.com>
2025-11-14 15:31:21 +0900

    temp

---
+++ src/components/layout/header/notification/NotificationErrorContent.tsx
@@ -0,0 +1,12 @@
+import RoundBox from '@/components/commonInGeneral/roundBox/RoundBox'
+
+const NotificationErrorContent = () => {
+  return (
+    <RoundBox className="h-[475px] w-[384px]">
+      <h2>Error</h2>
+      <p>잠시 후 다시 시도해주세요</p>
+    </RoundBox>
+  )
+}
+
+export default NotificationErrorContent
