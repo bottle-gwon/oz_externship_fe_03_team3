448410b7e382414f5827bccd5fb3a68adafd44d2
Tynahan <nusilite@gmail.com>
2025-11-14 15:48:57 +0900

    feat: 에러 컴포넌트 제작 후 적용 #372

--- src/components/layout/header/notification/NotificationErrorContent.tsx
+++ src/components/layout/header/notification/NotificationErrorContent.tsx
@@ -2,8 +2,11 @@ import RoundBox from '@/components/commonInGeneral/roundBox/RoundBox'
 
 const NotificationErrorContent = () => {
   return (
-    <RoundBox className="h-[475px] w-[384px]">
-      <h2>Error</h2>
+    <RoundBox
+      color="mono-dim"
+      className="gap-oz-xxl flex h-[475px] w-[384px] flex-col items-center justify-center"
+    >
+      <h2 className="text-primary-500 text-5xl font-bold">Error</h2>
       <p>잠시 후 다시 시도해주세요</p>
     </RoundBox>
   )
