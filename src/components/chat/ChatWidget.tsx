fe89c87600fb8536f920e0e6d649fddea717b11f
bottle-gwon <lazyguy1@daum.net>
2025-11-14 22:09:35 +0900

    feat: 채팅방 목록 열때, 새로운 메시지가 올때 채팅방 목록 다시 가져오기 #382

--- src/components/chat/ChatWidget.tsx
+++ src/components/chat/ChatWidget.tsx
@@ -3,14 +3,16 @@ import ChatList from './ChatList'
 import ChattingRoom from './ChattingRoom'
 import { useUnreadChatCount } from '@/hooks/chat/useChat'
 import { useEffect } from 'react'
+import { useQueryClient } from '@tanstack/react-query'
 
 // 레이아웃에서 출력용
 const ChatWidget = () => {
   const chatState = useStudyHubStore((state) => state.chatState)
   const accessToken = useStudyHubStore((state) => state.accessToken)
   const setUnReadCounter = useStudyHubStore((state) => state.setUnReadCounter)
+  const queryClient = useQueryClient()
 
-  const { data } = useUnreadChatCount()
+  const data = useUnreadChatCount()
 
   useEffect(() => {
     if (!accessToken) {
@@ -18,6 +20,15 @@ const ChatWidget = () => {
     }
     setUnReadCounter(data || 0)
   }, [data, setUnReadCounter, accessToken])
+
+  useEffect(() => {
+    if (chatState.status === 'chatList') {
+      queryClient.invalidateQueries({
+        queryKey: ['/chat'],
+      })
+    }
+  }, [chatState, queryClient])
+
   if (chatState.status === 'off') {
     return
   }
