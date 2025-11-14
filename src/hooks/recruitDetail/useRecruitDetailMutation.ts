3126b4c4f42b8461eaea2cdb071e3f2c7ce22c24
Tynahan <nusilite@gmail.com>
2025-11-14 20:27:11 +0900

    feat: 북마크 수정 시, 다른 캐시도 함께 무효화 #376

--- src/hooks/recruitDetail/useRecruitDetailMutation.ts
+++ src/hooks/recruitDetail/useRecruitDetailMutation.ts
@@ -1,8 +1,10 @@
 import type { RecruitDetail } from '@/types'
 import useSimpleMutation from '../useSimpleMutation'
 import api from '@/api/api'
+import { useQueryClient } from '@tanstack/react-query'
 
 const useRecruitDetailMutation = (recruitDetail: RecruitDetail) => {
+  const queryClient = useQueryClient()
   const queryEndpoint = `/recruitments/${recruitDetail.uuid}`
   const toggleBookmarkMutation = useSimpleMutation({
     queryEndpoint,
@@ -10,6 +12,10 @@ const useRecruitDetailMutation = (recruitDetail: RecruitDetail) => {
       api.post(`/recruitments/bookmarks/${recruitDetail.uuid}`),
     updateCacheForUi: (_previous: RecruitDetail, newOne: RecruitDetail) => {
       return newOne
+    },
+    handleSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['/recruitments'] })
+      queryClient.invalidateQueries({ queryKey: ['/recruitments/mine'] })
     },
   })
 
